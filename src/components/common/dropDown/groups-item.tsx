729ef6d063c0b4453b0c0e021f38c82bb6fb885d
nerte <grimza@nerteui-MacBookPro.local>
2025-04-20 14:43:40 +0900

    Style : groups-item.tsx styling

--- src/components/common/dropDown/groups-item.tsx
+++ src/components/common/dropDown/groups-item.tsx
@@ -2,22 +2,24 @@ import Link from 'next/link';
 import kebabIcon from '@/../public/icons/kebab-icon.svg';
 import Image from 'next/image';
 
-interface Props {
-  image: string;
-  id: number;
-  name: string;
+interface DropDownGroupItemPropsType {
+  group: Group;
 }
+export default function DropDownGroupsItem({ group }: DropDownGroupItemPropsType) {
+  const { name, image, id } = group;
 
-export default function DropDownGroupsItem({ image, id, name }: Props) {
   return (
-    <Link className="cursor-pointer" href={`/${id}`}>
-      <div>
+    <Link
+      className="text-lg-md text-gray400 hover:bg-bg100 flex w-[186px] cursor-pointer items-center justify-between rounded-lg px-[8px] py-[7px]"
+      href={`/${id}`}
+    >
+      <div className="flex items-center gap-3">
         <img src={image} className="h-8 w-8 rounded-md" />
-        {name}
-        <Link href={`/${id}/edit`}>
-          <Image width={2} height={8} src={kebabIcon} alt=":" />
-        </Link>
+        <p className="w-[110px] truncate overflow-hidden whitespace-nowrap"> {name}</p>
       </div>
+      <Link href={`/${id}/edit`}>
+        <Image width={16} height={16} src={kebabIcon} alt=":" />
+      </Link>
     </Link>
   );
 }
