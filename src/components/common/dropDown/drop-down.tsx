8a40c6d4ef1d8669dc238d73a6a6a66eb7debfa3
nerte <grimza@nerteui-MacBookPro.local>
2025-04-20 15:07:50 +0900

    Feat : groupListItem 컴포넌트를 기존 taskList 드롭다운에 적용

--- src/components/common/dropDown/drop-down.tsx
+++ src/components/common/dropDown/drop-down.tsx
@@ -6,23 +6,25 @@ import kebabIcon from '@/../public/icons/kebab-icon.svg';
 import dropDownIcon from '@/../public/icons/dropdown-icon.svg';
 import taskListIcon from '@/../public/icons/tasklist-icon.svg';
 import clsx from 'clsx';
+import DropDownGroupsItem from './groups-item';
+import Link from 'next/link';
 
 const ITEM_DROPDOWN_VALUE = ['수정하기', '삭제하기'];
 const PROFILE_DROPDOWN_VALUE = ['마이 히스토리', '계정 설정', '팀 참여', '로그아웃'];
 const RECURRING_DROPDOWN_VALUE = ['반복안함', '한번', '매일', '주 반복', '월 반복'];
 const SORT_DROPDOWN_VALUE = ['최신순', '좋아요 많은순'];
 
-type ArrayType = 'profile' | 'item' | 'recurring' | 'taskList' | 'sort';
+type ArrayType = 'profile' | 'item' | 'recurring' | 'groupList' | 'sort';
 
 interface DropDownProps {
   onClick: (value: string) => void;
   value: ArrayType;
-  taskList?: string[];
+  groupList?: Group[];
   openBtn?: ReactNode;
 }
 
 //기본 모달 (케밥,프로필 클릭시 나오는 )
-export function DropDown({ onClick, openBtn, value, taskList }: DropDownProps) {
+export function DropDown({ onClick, openBtn, value, groupList }: DropDownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const ref = useRef<HTMLUListElement>(null);
   let options;
@@ -37,11 +39,9 @@ export function DropDown({ onClick, openBtn, value, taskList }: DropDownProps) {
     case 'recurring':
       options = RECURRING_DROPDOWN_VALUE;
       break;
-    case 'taskList':
-      options = taskList;
-      break;
     case 'sort':
       options = SORT_DROPDOWN_VALUE;
+      break;
   }
 
   useEffect(() => {
@@ -80,29 +80,39 @@ export function DropDown({ onClick, openBtn, value, taskList }: DropDownProps) {
             value === 'profile' && 'sm:text-md-rg md:text-lg-rg w-30 md:w-[135px]',
             value === 'item' && 'text-md-rg w-30',
             value === 'recurring' && 'text-md-rg w-[109px]',
-            value === 'taskList' && 'text-lg-md max-h-[300px] w-[218px] px-4 py-4',
+            value === 'groupList' && 'text-lg-md max-h-[300px] w-[218px] px-4 py-4',
             value === 'sort' && 'sm:text-xs-rg md:text-md-rg sm:w-[94px] md:w-30'
           )}
           ref={ref}
         >
-          {options?.map((option) => {
-            return (
-              <li
-                className={clsx(
-                  'hover:bg-bg100 flex items-center justify-center rounded-xl',
-                  value === 'profile' && 'py-[14px]',
-                  value === 'item' && 'py-3',
-                  value === 'recurring' && 'py-3',
-                  value === 'taskList' && 'h-[46px]',
-                  value === 'sort' && 'py-[13px]'
-                )}
-                onClick={handleClickOption}
-                key={option}
-              >
-                {option}
-              </li>
-            );
-          })}
+          {!groupList ? (
+            <>
+              {options?.map((option) => {
+                return (
+                  <li
+                    className={clsx(
+                      'hover:bg-bg100 flex items-center justify-center rounded-xl',
+                      value === 'profile' && 'py-[14px]',
+                      value === 'item' && 'py-3',
+                      value === 'recurring' && 'py-3',
+                      value === 'sort' && 'py-[13px]'
+                    )}
+                    onClick={handleClickOption}
+                    key={option}
+                  >
+                    {option}
+                  </li>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              {groupList.map((group) => {
+                return <DropDownGroupsItem group={group} />;
+              })}
+              <Link href="/addteam">팀추가버튼시교체</Link>
+            </>
+          )}
         </ul>
       )}
     </>
@@ -112,18 +122,18 @@ export function DropDown({ onClick, openBtn, value, taskList }: DropDownProps) {
 interface SelectedDropDownProps {
   onClick: (value: string) => void;
   value: ArrayType;
-  taskList?: string[];
+  groupList?: Group[];
   selected?: string;
 }
 
 //셀렉티드 모달 (최신-좋아요순 정렬 / 헤더의 태스크 드롭다운 / 반복일정)
-export function SelectedDropDown({ onClick, value, taskList, selected }: SelectedDropDownProps) {
+export function SelectedDropDown({ onClick, value, groupList, selected }: SelectedDropDownProps) {
   const defaultValue =
     value === 'recurring'
       ? RECURRING_DROPDOWN_VALUE[0]
       : value === 'sort'
         ? SORT_DROPDOWN_VALUE[0]
-        : taskList![0];
+        : groupList![0].name;
 
   const [currentSelected, setCurrentSelected] = useState(selected ?? defaultValue);
 
@@ -140,7 +150,7 @@ export function SelectedDropDown({ onClick, value, taskList, selected }: Selecte
             className={clsx(
               'text-gray100 flex items-center rounded-xl',
               value === 'recurring' && 'bg-bg400 text-gray500 h-11 w-[109px] px-3 py-[10px]',
-              value === 'taskList' && 'bg-bg200 h-fit w-fit',
+              value === 'groupList' && 'bg-bg200 h-fit w-fit',
               value === 'sort' &&
                 'sm:text-xs-rg md:text-md-rg bg-bg200 px-2 py-2 sm:h-10 sm:w-[94px] md:h-11 md:w-30'
             )}
@@ -148,11 +158,13 @@ export function SelectedDropDown({ onClick, value, taskList, selected }: Selecte
             <div
               className={clsx(
                 'flex w-full items-center',
-                value === 'taskList' ? 'gap-[11px]' : 'justify-between'
+                value === 'groupList' ? 'gap-[11px]' : 'justify-between'
               )}
             >
-              {currentSelected}
-              {value !== 'taskList' ? (
+              <p className="w-[110px] truncate overflow-hidden whitespace-nowrap">
+                {currentSelected}
+              </p>
+              {value !== 'groupList' ? (
                 <Image width={24} height={24} src={dropDownIcon} alt="\/" />
               ) : (
                 <Image width={16} height={16} alt="^" src={taskListIcon} />
@@ -162,7 +174,7 @@ export function SelectedDropDown({ onClick, value, taskList, selected }: Selecte
         }
         onClick={handleClickOption}
         value={value}
-        taskList={taskList}
+        groupList={groupList}
       />
     </>
   );
