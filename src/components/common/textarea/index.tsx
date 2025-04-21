8813951fc0831b697112b72c3fe44aa5007a482f
heejin <heejin0857@naver.com>
2025-04-22 02:36:26 +0900

    Feat : borderNone props & height logic simplify

--- src/components/common/textarea/index.tsx
+++ src/components/common/textarea/index.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import { useRef, useEffect, useCallback } from 'react';
-import { TextareaHTMLAttributes } from 'react';
+import { useRef, useEffect, useCallback, TextareaHTMLAttributes } from 'react';
 import debounce from 'lodash.debounce';
 import clsx from 'clsx';
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   rightSlot?: React.ReactNode;
   height?: number;
+  borderNone?: boolean;
   className?: string;
 }
 
 export default function Textarea({
   rightSlot = null,
   height = 24,
+  borderNone = false,
   className,
   ...rest
 }: TextareaProps) {
@@ -21,10 +22,9 @@ export default function Textarea({
 
   const handleTextareaHeight = useCallback(() => {
     const textarea = textareaRef.current;
-    if (textarea) {
-      const initialHeight = height && height !== 24 ? `${height}px` : '24px';
 
-      textarea.style.height = initialHeight;
+    if (textarea) {
+      textarea.style.height = `${height}px`;
       textarea.style.height = `${textarea.scrollHeight}px`;
     }
   }, [height]);
@@ -43,19 +43,20 @@ export default function Textarea({
   return (
     <div
       className={clsx(
-        'bg-bg200 border-border flex w-full items-start gap-2 rounded-xl border px-4 py-2 sm:px-6 sm:py-4'
+        'bg-bg200 flex w-full items-start gap-2',
+        !borderNone && 'border-border rounded-xl border px-4 py-2 sm:px-6 sm:py-4'
       )}
     >
       <textarea
         ref={textareaRef}
         onInput={handleTextareaHeight}
         className={clsx(
-          'placeholder:text-gray500 text-md-rg sm:text-lg-rg placeholder:text-md-rg sm:placeholder:text-lg-rg flex max-h-60 w-full resize-none items-center justify-center focus:outline-none',
+          'placeholder:text-gray500 text-md-rg sm:text-lg-rg placeholder:text-md-rg sm:placeholder:text-lg-rg flex max-h-60 w-full resize-none items-center justify-center pt-1 focus:outline-none',
           className
         )}
         {...rest}
         style={{
-          height: height ? `${height}px` : '24px',
+          height: `${height}px`,
         }}
       />
       {rightSlot}
