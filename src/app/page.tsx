c14276f90eeeab5da7977914f262ed9684d7e9f2
hyejin <hhyejin90@gmail.com>
2025-04-21 19:16:08 +0900

    Chore : user.svg 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import HeaderDefault from '@/components/layout/gnb/HeaderDefault';
+import Header from '@/components/layout/gnb/Header';
 
 export default function Home() {
+<<<<<<< HEAD
   return <div>인덱스 페이지</div>;
+=======
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+>>>>>>> 42d0bd3 (Chore : user.svg 추가)
 }
--- src/components/layout/gnb/Header.tsx
+++ src/components/layout/gnb/Header.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Header.tsx
 'use client';
 
@@ -209,3 +210,33 @@ export default function Header() {
     </header>
   );
 }
+=======
+'use client ';
+
+import Image from 'next/image';
+import logoLg from '@/assets/logo-lg.svg';
+import logoSm from '@/assets/logo-sm.svg';
+
+export default function Header() {
+  return (
+    <header className="bg-bg200 sticky top-0 h-[60px] w-full">
+      <div className="flex h-full w-full items-center pl-[16px] md:pl-[24px] lg:pl-[360px]">
+        <Image
+          src={logoLg}
+          alt="Coworkers logo"
+          width={158}
+          height={32}
+          className="hidden lg:block"
+        />
+        <Image
+          src={logoSm}
+          alt="Coworkers logo"
+          width={102}
+          height={20}
+          className="block lg:hidden"
+        />
+      </div>
+    </header>
+  );
+}
+>>>>>>> 42d0bd3 (Chore : user.svg 추가)
