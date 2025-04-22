2d7924a436fb23111d060b8be2bd4ae9490f65d3
heejin <heejin0857@naver.com>
2025-04-23 04:30:29 +0900

    Feat : common task list UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import TaskList from '@/components/task-list/TaskList';
+
 export default function Home() {
-  return <div>인덱스 페이지</div>;
+  return (
+    <div>
+      <TaskList />
+    </div>
+  );
 }
---
+++ src/components/task-list/TaskList.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+
+// doneAt이 빈 스트링이라도 내려오는 값인지 확인
+// 매일 반복 클릭하면 색상 primary 컬러로 바뀌도록 : svgr 추가되면 작업 예정
+
+export default function TaskList({
+  doneAt = '',
+  description = '이것저것 다 하기',
+  commentCount = 3,
+  date = '2025년 04월 23일',
+}) {
+  const checkIcon = doneAt === '' ? '/icons/none-check-box.svg' : '/icons/check-box.svg';
+
+  return (
+    <div className="bg-bg200 text-xs-rg text-gray500 flex w-full flex-col gap-2.5 rounded-lg px-[14px] py-3">
+      <div className="flex justify-between">
+        <div className="flex w-fit items-center justify-start gap-2">
+          <Image src={checkIcon} width={24} height={24} alt="checked" />
+          <span className="text-md-rg text-gray100 pt-0.5">{description}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-0.5">
+            <Image src="/icons/comment.svg" width={16} height={16} alt="comment" />
+            <span className="pt-0.5">{commentCount}</span>
+          </div>
+          <Image
+            src="/icons/kebab-icon.svg"
+            width={16}
+            height={16}
+            alt="kebab"
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      <div className="flex items-center">
+        <div className="flex items-center gap-1.5">
+          <Image src="/icons/calendar.svg" width={16} height={16} alt="calendar" />
+          <span className="pt-0.5">{date}</span>
+        </div>
+        <div className="bg-bg100 mx-2.5 h-2 w-0.25" />
+        <div className="flex items-center gap-1.5">
+          <div className="bg-bg100 h-4 w-4" />
+          <span className="pt-0.5">매일 반복</span>
+        </div>
+      </div>
+    </div>
+  );
+}
