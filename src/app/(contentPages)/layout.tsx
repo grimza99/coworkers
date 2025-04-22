b736731801333b676486546ed03358b2cc3609e5
nerte <grimza@nerteui-MacBookPro.local>
2025-04-23 00:41:23 +0900

    Style : 중복되는 스타일 제거

--- src/app/(contentPages)/layout.tsx
+++ src/app/(contentPages)/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <div className="flex justify-center">
-          <div className="mx-4 mt-6 h-dvh w-full max-w-300 min-w-[343px] sm:mx-4 md:mx-6">
-            {children}
-          </div>
+          <div className="mx-4 mt-6 h-dvh w-full max-w-300 min-w-[343px] md:mx-6">{children}</div>
         </div>
       </body>
     </html>
