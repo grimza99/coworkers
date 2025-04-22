0ba85bb6e2d1bda183c6fe06379a5aadb3a479f4
nerte <grimza@nerteui-MacBookPro.local>
2025-04-22 18:47:00 +0900

    Refactor : 컴포넌트 이름 변경 OAuthUI -> OAuth ,Enum type authType  설정

--- src/components/oauth/OAuthBtnGroup.tsx
+++ src/components/oauth/OAuthBtnGroup.tsx
@@ -3,15 +3,19 @@ import Image from 'next/image';
 import kakaoIcon from '@/../public/icons/kakao-oauth-icon.svg';
 import googleIcon from '@/../public/icons/google-oauth-icon.svg';
 
-export default function OAuthBtnGroup() {
+export default function OAuthButtonGroup() {
   const handleClick = () => {
     //OAuth 함수구현
   };
 
   return (
     <div className="flex gap-4">
-      <Image onClick={handleClick} src={googleIcon} width={42} height={42} alt="구글" />
-      <Image onClick={handleClick} src={kakaoIcon} width={42} height={42} alt="카카오" />
+      <button>
+        <Image onClick={handleClick} src={googleIcon} width={42} height={42} alt="구글" />
+      </button>
+      <button>
+        <Image onClick={handleClick} src={kakaoIcon} width={42} height={42} alt="카카오" />
+      </button>
     </div>
   );
 }
--- src/components/oauth/index.tsx
+++ src/components/oauth/index.tsx
@@ -1,11 +1,11 @@
-import OAuthBtnGroup from './OAuthBtnGroup';
+import OAuthButtonGroup from './OAuthBtnGroup';
 
 interface OAuthUIProps {
-  login?: boolean;
-  signUp?: boolean;
+  authType: 'login' | 'signup';
 }
 
-export default function OAuthUI({ login, signUp }: OAuthUIProps) {
+export default function OAuth({ authType }: OAuthUIProps) {
+  const authText = authType === 'login' ? '간편 로그인하기' : '간편 회원가입하기';
   return (
     <div className="flex flex-col gap-4 text-white">
       <div className="flex items-center gap-6">
@@ -14,9 +14,8 @@ export default function OAuthUI({ login, signUp }: OAuthUIProps) {
         <div className="border-border h-[1px] w-full border-1" />
       </div>
       <div className="flex justify-between">
-        {login && <p className="text-lg-md">간편 로그인하기</p>}
-        {signUp && <p className="text-lg-md">간편 회원가입하기</p>}
-        <OAuthBtnGroup />
+        <p className="text-lg-md">{authText}</p>
+        <OAuthButtonGroup />
       </div>
     </div>
   );
