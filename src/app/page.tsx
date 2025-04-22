3913b8b2501286a095e10d3dee7983c2791c4b93
hyejin <hhyejin90@gmail.com>
2025-04-22 18:52:57 +0900

    Fix : 컨플릭트 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Header from '@/components/layout/gnb/Header';
 
 export default function Home() {
-<<<<<<< HEAD
-  return <div>인덱스 페이지</div>;
-=======
   return (
     <div>
       <Header />
     </div>
   );
->>>>>>> 42d0bd3 (Chore : user.svg 추가)
 }
