15846b444d23ee384cd951896cee5e3616b0b23b
nerte <grimza@nerteui-MacBookPro.local>
2025-04-23 17:04:08 +0900

    Chore : style 수정

--- src/app/(form-layout)/layout.tsx
+++ src/app/(form-layout)/layout.tsx
@@ -7,7 +7,7 @@ export default function FormPageLayout({
 }>) {
   return (
     <div className="flex justify-center">
-      <div className="mx-4 mt-6 flex h-dvh w-full max-w-115 min-w-[343px] justify-center md:mx-6 md:mt-25 lg:mt-35">
+      <div className="mx-4 mt-6 flex h-dvh w-full max-w-115 min-w-[343px] md:mx-6 md:mt-25 lg:mt-35">
         {children}
       </div>
     </div>
