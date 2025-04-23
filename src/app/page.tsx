91252e0a98f7aceab797e07fe1afebfa2400e27d
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-23 23:00:32 +0900

    Feat : 홈페이지 시작 버튼 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import StartButton from '@/components/pages/home/StartButton';
+
 export default function Home() {
-  return <div>인덱스페이지</div>;
+  return (
+    <div className="m-auto flex h-screen w-full items-center justify-center">
+      <StartButton className="text-lg-semi bg-primary flex h-12 justify-center rounded-4xl px-[143px] py-[14.5px]">
+        지금 시작하기
+      </StartButton>
+    </div>
+  );
 }
