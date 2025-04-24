ab543e4db123fcadbe23a59fe6d90a79efea9d49
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-24 16:03:32 +0900

    Feat : 기본 로그인 폼 UI 구현

--- src/app/(form-layout)/layout.tsx
+++ src/app/(form-layout)/layout.tsx
@@ -7,6 +7,7 @@ export default function FormPageLayout({
 }>) {
   return (
     <div className="flex justify-center">
+      {/* h-dvh 클래스 유무에 따라 상단 마진이 크게 바뀌는 것 같습니다. 필요한 클래스일까요? - @KSJ27 */}
       <div className="mx-4 mt-6 flex h-dvh w-full max-w-115 min-w-[343px] md:mx-6 md:mt-25 lg:mt-35">
         {children}
       </div>
--- src/app/(form-layout)/login/page.tsx
+++ src/app/(form-layout)/login/page.tsx
@@ -1,3 +1,33 @@
+import Input from '@/components/common/formField/compound/Input';
+import OAuth from '@/components/oauth';
+
 export default function Home() {
-  return <div className="bg-bg200 h-10 w-full">레이아웃 테스트를 위한 div입니다.</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="m-auto mb-10 flex w-[460px] flex-col items-center">
+        <h1 className="mb-20 text-4xl">로그인</h1>
+        <form className="flex w-full flex-col gap-4">
+          <div>
+            <label htmlFor="email" className="mb-2">
+              이메일
+            </label>
+            <Input id="email" type="email" placeholder="이메일을 입력해주세요." />
+          </div>
+          <div>
+            <label htmlFor="password" className="mb-2">
+              비밀번호
+            </label>
+            <Input id="password" type="password" placeholder="비밀번호를 입력해주세요." />
+          </div>
+          <button
+            type="submit"
+            className="text-lg-semi bg-primary flex h-12 justify-center rounded-xl px-4 py-3"
+          >
+            로그인
+          </button>
+        </form>
+      </div>
+      <OAuth authType="login" />
+    </div>
+  );
 }
