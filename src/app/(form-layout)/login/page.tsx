5e5b009fe32a4faf435850c8ce5a418e656d9949
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-24 21:43:27 +0900

    Fix : login page 레이아웃 수정

--- src/app/(form-layout)/login/page.tsx
+++ src/app/(form-layout)/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from './_login/LoginForm';
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="m-auto mb-10 flex w-[460px] flex-col items-center">
+      <div className="mb-10 flex w-[460px] flex-col items-center">
         <h1 className="mb-20 text-4xl">로그인</h1>
         <LoginForm />
       </div>
