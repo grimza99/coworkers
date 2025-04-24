6518b08918f45504fa3a26bccdd0e9549e157848
nerte <grimza@nerteui-MacBookPro.local>
2025-04-24 17:07:59 +0900

    Feat : create teamId/tasklist page

---
+++ src/app/(content-layout)/[teamid]/tasklist/page.tsx
@@ -0,0 +1,12 @@
+import HandleDate from './_tasklist/components/HandleDate';
+
+export default function Page() {
+  return (
+    <div>
+      <p>할일</p>
+      <p>날짜</p>
+      <HandleDate />
+      <div>리스트 페이지</div>
+    </div>
+  );
+}
