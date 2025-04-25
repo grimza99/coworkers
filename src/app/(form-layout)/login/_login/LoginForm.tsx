d035c29fd9263794de151cd03707be990e22e06b
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-25 15:27:04 +0900

    Refactor : LoginForm의 입력창을 FormField 컴포넌트로 변경

--- src/app/(form-layout)/login/_login/LoginForm.tsx
+++ src/app/(form-layout)/login/_login/LoginForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useActionState, useState } from 'react';
-import Input from '@/components/common/formField/compound/Input';
 import login from './login';
+import FormField from '@/components/common/formField';
 
 export default function LoginForm() {
   // @TODO: 입력 변화에 따른 UI 변화, 입력값 검증
@@ -16,34 +16,30 @@ export default function LoginForm() {
 
   return (
     <form action={loginAction} className="flex w-full flex-col gap-4">
-      <div>
-        <label htmlFor="email" className="mb-2">
-          이메일
-        </label>
-        <Input
-          id="email"
-          type="email"
-          name="email"
-          value={email}
-          placeholder="이메일을 입력해주세요."
-          onChange={(e) => setEmail(e.target.value)}
-          disabled={isPending}
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="mb-2">
-          비밀번호
-        </label>
-        <Input
-          id="password"
-          type="password"
-          name="password"
-          value={password}
-          placeholder="비밀번호를 입력해주세요."
-          onChange={(e) => setPassword(e.target.value)}
-          disabled={isPending}
-        />
-      </div>
+      <FormField
+        name="email"
+        label="이메일"
+        type="email"
+        textField="input"
+        placeholder="이메일을 입력해주세요."
+        value={email}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setEmail(e.target.value);
+        }}
+        disabled={isPending}
+      />
+      <FormField
+        name="password"
+        label="비밀번호"
+        type="password"
+        textField="input"
+        placeholder="비밀번호를 입력해주세요."
+        value={password}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setPassword(e.target.value);
+        }}
+        disabled={isPending}
+      />
       <button
         type="submit"
         className="text-lg-semi bg-primary disabled:bg-gray400 flex h-12 justify-center rounded-xl px-4 py-3"
