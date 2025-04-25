95d30300bc7898d5293aa8ec89661529349f8a8d
nerte <grimza@nerteui-MacBookPro.local>
2025-04-25 20:12:58 +0900

    Fix : svgr 로 수정후 컴포넌트로 변경

--- src/components/layout/gnb/Logo.tsx
+++ src/components/layout/gnb/Logo.tsx
@@ -1,26 +1,13 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import logoLg from '@/assets/logo-lg.svg';
-import logoSm from '@/assets/logo-sm.svg';
+import LogoLg from '@/assets/logo-lg.svg';
+import LogoSm from '@/assets/logo-sm.svg';
 
 export default function Logo() {
   return (
     <div>
       <Link href="/" className="flex items-center">
-        <Image
-          src={logoLg}
-          alt="Coworkers logo"
-          width={158}
-          height={32}
-          className="hidden lg:block"
-        />
-        <Image
-          src={logoSm}
-          alt="Coworkers logo"
-          width={102}
-          height={20}
-          className="block lg:hidden"
-        />
+        <LogoLg className="hidden lg:block" />
+        <LogoSm className="block lg:hidden" />
       </Link>
     </div>
   );
