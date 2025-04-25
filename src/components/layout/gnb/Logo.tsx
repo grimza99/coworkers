b7bb69b9f245b29f2151a10ad82288a2b0b451db
hyejin <hhyejin90@gmail.com>
2025-04-25 22:55:57 +0900

    Style : logo 파일명 변경 후로고  반응형 코드 수정

--- src/components/layout/gnb/Logo.tsx
+++ src/components/layout/gnb/Logo.tsx
@@ -1,26 +1,11 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import logoLg from '@/assets/logo-lg.svg';
-import logoSm from '@/assets/logo-sm.svg';
+import CoworkersLogo from '@/assets/CoworkersLogo.svg';
 
 export default function Logo() {
   return (
-    <div>
-      <Link href="/" className="flex items-center">
-        <Image
-          src={logoLg}
-          alt="Coworkers logo"
-          width={158}
-          height={32}
-          className="hidden lg:block"
-        />
-        <Image
-          src={logoSm}
-          alt="Coworkers logo"
-          width={102}
-          height={20}
-          className="block lg:hidden"
-        />
+    <div className="h-5 w-[102px] lg:h-8 lg:w-[158px]">
+      <Link href="/">
+        <CoworkersLogo />
       </Link>
     </div>
   );
