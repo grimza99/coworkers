a7b4d95a5a8144f9e8bbb7c706e7f6b3dc5c020d
nerte <grimza@nerteui-MacBookPro.local>
2025-04-25 22:39:20 +0900

    Feat : create handleDate component

---
+++ src/app/(content-layout)/[teamid]/tasklist/_tasklist/HandleDate.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from 'react';
+
+import prevIcon from '@/../public/icons/prev-arrow-icon.svg';
+import nextIcon from '@/../public/icons/next-arrow-icon.svg';
+import calendar from '@/../public/icons/calendar.icon.svg';
+
+import Image from 'next/image';
+import { addDays, format, subDays } from 'date-fns';
+
+export default function HandleDate() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const formattedDateToKorean = format(currentDate, 'M월 dd일');
+
+  const handleClickChangeDayIcon = (value: string) => {
+    if (value === 'prev') {
+      setCurrentDate((prev) => subDays(prev, 1));
+    } else {
+      setCurrentDate((prev) => addDays(prev, 1));
+    }
+  };
+
+  const calendarPop = () => {
+    // 캘린더 띄움
+  };
+
+  return (
+    <div className="flex items-center gap-3">
+      <p className="text-lg-md">{formattedDateToKorean}</p>
+      <div className="flex gap-1">
+        <button onClick={() => handleClickChangeDayIcon('prev')}>
+          <Image src={prevIcon} width={16} height={16} alt="<" />
+        </button>
+        <button onClick={() => handleClickChangeDayIcon('next')}>
+          <Image src={nextIcon} width={16} height={16} alt=">" />
+        </button>
+      </div>
+
+      <button onClick={calendarPop}>
+        <Image src={calendar} width={24} height={24} alt=">" />
+      </button>
+    </div>
+  );
+}
---
+++ src/app/(content-layout)/[teamid]/tasklist/page.tsx
@@ -0,0 +1,11 @@
+import HandleDate from './_tasklist/HandleDate';
+
+export default function Page() {
+  return (
+    <div>
+      <p>할일</p>
+      <HandleDate />
+      <div>리스트 페이지</div>
+    </div>
+  );
+}
