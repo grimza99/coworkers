d8a0ed42145c0d213aef1fe1b1c31ea30eec7743
heejin <heejin0857@naver.com>
2025-04-27 05:22:08 +0900

    Feat : apply FormField & button

--- src/app/(form-layout)/addteam/page.tsx
+++ src/app/(form-layout)/addteam/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import Button from '@/components/common/Button';
 import FormField from '@/components/common/formField';
 
 export default function AddTeam() {
@@ -6,15 +9,29 @@ export default function AddTeam() {
       <h1 className="text-4xl">팀 수정하기</h1>
       <form className="flex w-full flex-col gap-10">
         <div className="flex w-full flex-col gap-6">
-          <FormField textField="input" label="팀 프로필" gapSize="24" labelSize="16/16" />
           <FormField
-            textField="input"
+            field="file-input"
+            label="팀 프로필"
+            gapSize="24"
+            FileInputUsage="team"
+            image=""
+            onImageChange={() => {}}
+          />
+          <FormField
+            field="input"
             label="팀 이름"
             gapSize="24"
             placeholder="팀 이름을 입력해 주세요."
           />
         </div>
-        <button className="bg-primary text-white">asdf</button>
+        <div className="flex flex-col gap-6">
+          <Button type="submit" variant="solid" size="fullWidth">
+            생성하기
+          </Button>
+          <p className="text-lg-rg text-gray500 text-center">
+            팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
+          </p>
+        </div>
       </form>
     </div>
   );
