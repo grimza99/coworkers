03aedebacca8276f55696ce11e0528195e4a8ac8
nerte <grimza@nerteui-MacBookPro.local>
2025-04-28 19:02:24 +0900

    Chore : 삼항연산자 순서 바꿈

--- src/app/(content-layout)/myhistory/page.tsx
+++ src/app/(content-layout)/myhistory/page.tsx
@@ -8,12 +8,12 @@ export default function MyHistoryPage() {
   return (
     <div className="flex flex-col gap-6 md:gap-[27px]">
       <h3 className="text-xl-bold">마이 히스토리</h3>
-      {!data ? (
+      {data ? (
+        <GroupedByDateTaskList historyTaskData={data} />
+      ) : (
         <div className="flex h-svh w-full items-center justify-center">
           <p className="text-md-md text-gray-500">아직 히스토리가 없습니다.</p>
         </div>
-      ) : (
-        <GroupedByDateTaskList historyTaskData={data} />
       )}
     </div>
   );
