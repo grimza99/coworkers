5dd863a1bebd03a1b6ae421761bf2ba26cce7180
hyejin <hhyejin90@gmail.com>
2025-04-28 09:39:19 +0900

    Chore : validators.ts에 todo 추가

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,20 +1,20 @@
-// src/utils/validators.ts
-
 export const validateEmail = (email: string) => {
+  // @TODO: 이메일 형식, 중복된 이메일인지도 체크
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
 export const validatePassword = (password: string) => {
-  // 예시: 비밀번호 8자 이상
+  // @TODO: 비밀번호 8자리 이상, 영어 대문자와 특수기호 포함 (지금은 8자리만 체크)
   return password.length >= 8;
 };
 
 export const validateConfirmPassword = (password: string, confirmPassword: string) => {
+  // @TODO: 비밀번호와 일치하는지 체크(지금은 길이만 됨)
   return password === confirmPassword && confirmPassword.length > 0;
 };
 
 export const validateName = (name: string) => {
-  // 이름이 비어있지 않은지만 체크
+  // @TODO: 트림, 중복된 이름인지도 체크
   return name.trim().length > 0;
 };
