933914bbf956a1c1e8a486b8ba8a4f77d1fe46d3
nerte <grimza@nerteui-MacBookPro.local>
2025-04-28 17:58:48 +0900

    Refactor : early return 으로 data 없을때 처리

--- src/app/(content-layout)/myhistory/page.tsx
+++ src/app/(content-layout)/myhistory/page.tsx
@@ -4,17 +4,17 @@ import { HISTORY_MOCK_DATA } from './_myhistory/myhistory-mock-data';
 export default function MyHistoryPage() {
   const data = HISTORY_MOCK_DATA; // 마이 히스토리 페이지에 필요한 데이터 불러와서 담을 변수
   // const data = ''; //data가 없을 경우
+  if (!data)
+    return (
+      <div className="flex h-svh w-full items-center justify-center">
+        <p className="text-md-md text-gray-500">아직 히스토리가 없습니다.</p>
+      </div>
+    );
 
   return (
     <div className="flex flex-col gap-6 md:gap-[27px]">
       <h3 className="text-xl-bold">마이 히스토리</h3>
-      {!data ? (
-        <div className="flex h-svh w-full items-center justify-center">
-          <p className="text-md-md text-gray-500">아직 히스토리가 없습니다.</p>
-        </div>
-      ) : (
-        <GroupedByDateTaskList historyTaskData={data} />
-      )}
+      <GroupedByDateTaskList historyTaskData={data} />
     </div>
   );
 }
