c5f291dc7d56381b7abb3f37736ffe864bf88d43
hyejin <hhyejin90@gmail.com>
2025-04-28 15:04:49 +0900

    Feat : validatePassword 함수에 대소문자 및 특수문자 포함 조건 추가

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,12 +1,12 @@
 export const validateEmail = (email: string) => {
-  // @TODO: 이메일 형식, 중복된 이메일인지도 체크
+  // @TODO: 중복된 이메일인지 체크
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
 export const validatePassword = (password: string) => {
-  // @TODO: 비밀번호 8자리 이상, 영어 대문자와 특수기호 포함 (지금은 8자리만 체크)
-  return password.length >= 8;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_\-+=~`[\]{}|\\:;"'<>,.?/]).{8,}$/;
+  return passwordRegex.test(password);
 };
 
 export const validateConfirmPassword = (password: string, confirmPassword: string) => {
