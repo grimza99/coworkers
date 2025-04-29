0d8f9162f9b528de63fe8bbdad8d2db051e46c45
hyejin <hhyejin90@gmail.com>
2025-04-29 22:41:17 +0900

    Feat : 탈퇴하기 버튼 추가

--- src/app/(form-layout)/mypage/page.tsx
+++ src/app/(form-layout)/mypage/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+import Button from '@/components/common/Button';
 import FormField from '@/components/common/formField';
 
 export default function MyTeam() {
@@ -15,11 +17,29 @@ export default function MyTeam() {
           onImageChange={() => {}}
         />
         <div className="flex w-full flex-col gap-6">
-          <FormField field="input" label="이름" />
+          <FormField
+            field="input"
+            label="이름"
+            rightSlot={
+              <Button size="xs" fontSize="14">
+                변경하기
+              </Button>
+            }
+          />
           <FormField field="input" label="이메일" />
-          <FormField field="input" type="password" label="비밀번호" />
+          <FormField
+            field="input"
+            type="password"
+            label="비밀번호"
+            rightSlot={
+              <Button size="xs" fontSize="14">
+                변경하기
+              </Button>
+            }
+          />
         </div>
-        <button type="submit" className="text-danger text-lg-md text-start">
+        <button type="submit" className="text-danger text-lg-md flex items-center gap-1 text-start">
+          <Image src="/icons/secession.svg" width={24} height={24} alt="회원탈퇴 아이콘" />
           회원 탈퇴하기
         </button>
       </form>
