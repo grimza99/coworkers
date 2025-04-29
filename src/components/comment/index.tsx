da59f6773f1849f32d107685e9b0dace2659cdb3
Seok-jun.Kang <kangsj0327@gmail.com>
2025-04-30 00:28:11 +0900

    Feat : Comment Dropdown 컴포넌트 추가

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
-import kebabIcon from '@/../public/icons/kebab-icon.svg';
 import { Comment } from './types';
 import { formatTimeDistance } from '@/utils/date';
-import ProfileBadge from '../profile-badge';
+import ProfileBadge from '@/components/profile-badge';
+import CommentItemDropdown from './CommentItemDropdown';
 
 interface CommentItemProps {
   comment: Comment;
@@ -14,9 +13,7 @@ export default function CommentItem({ comment }: CommentItemProps) {
     <div className="border-b-border flex flex-col gap-4 border-b-2 pb-4">
       <div className="flex items-start justify-between">
         <div className="text-md-rg break-keep whitespace-pre-wrap">{content}</div>
-        <button className="shrink-0">
-          <Image width="16" height="16" src={kebabIcon} alt={'메뉴 열기'} />
-        </button>
+        <CommentItemDropdown />
       </div>
       <div className="flex items-center justify-between gap-4">
         <ProfileBadge user={user} />
