e8d3408f888d9c9c1f8f0ebd9cf94b3ddcfee864
heejin <heejin0857@naver.com>
2025-04-30 05:02:45 +0900

    Feat : time picekr

--- src/components/manage-task-modal/ManageTaskModal.tsx
+++ src/components/manage-task-modal/ManageTaskModal.tsx
@@ -8,16 +8,53 @@ import FormField from '../common/formField';
 import { OptionSelector } from '../common/dropdown/OptionSelector';
 import CalendarSelect from '../calendar/CalendarSelect';
 import WeeklySelect from './WeeklySelect';
+import TimePicker from './TimePicker';
+import generateTime from './time-table';
 
 // 수정 작업 진행할 때 h1과 button children 분기 처리 예정
+
+export interface Time {
+  period: '오전' | '오후';
+  time: string;
+}
 
 const FREQUENCY_LIST = ['한 번', '매일', '주 반복', '월 반복'];
 
 export default function ManageTaskModal() {
+  const { am, pm } = generateTime();
+
   const [date, setDate] = useState(new Date());
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [isTimeOpen, setIsTimeOpen] = useState(false);
   const [selectedFrequency, setSelectedFrequency] = useState('');
   const [selectedDays, setSelectedDays] = useState<number[]>([]);
+  const [selectedTime, setSelectedTime] = useState<Time>({
+    period: '오전',
+    time: am[0],
+  });
+
+  const select = [
+    {
+      id: 'date',
+      value: format(date, 'yyyy년 MM월 dd일'),
+      onClick: () => {
+        setIsCalendarOpen((prev) => !prev);
+        setIsTimeOpen(false);
+      },
+      isOpen: isCalendarOpen,
+      flex: 'flex-[1.65]',
+    },
+    {
+      id: 'time',
+      value: `${selectedTime.period} ${selectedTime.time}`,
+      onClick: () => {
+        setIsTimeOpen((prev) => !prev);
+        setIsCalendarOpen(false);
+      },
+      isOpen: isTimeOpen,
+      flex: 'flex-[1]',
+    },
+  ];
 
   const handleCalendarDateChange = (selectedDate: Date) => {
     setDate(selectedDate);
@@ -28,6 +65,19 @@ export default function ManageTaskModal() {
     setSelectedDays((prev) =>
       prev.includes(idx) ? prev.filter((i) => i !== idx) : [...prev, idx]
     );
+  };
+
+  const updateTime = (key: 'period' | 'time', value: string) => {
+    setSelectedTime((prev) => {
+      if (key === 'period') {
+        const newTime = value === '오전' ? am[0] : pm[0];
+        return { period: value as '오전' | '오후', time: newTime };
+      }
+
+      setIsTimeOpen(false);
+
+      return { ...prev, [key]: value };
+    });
   };
 
   return (
@@ -47,21 +97,24 @@ export default function ManageTaskModal() {
         <div className="flex flex-col gap-2">
           <div className="flex flex-col gap-4">
             <label className="text-lg-md">시간 날짜 및 시간</label>
-            <span
-              onClick={() => setIsCalendarOpen((prev) => !prev)}
-              className={clsx(
-                'text-gray500 text-md-rg sm:text-lg-rg bg-bg200 flex h-11 w-full cursor-pointer items-center gap-3 rounded-xl border px-4 py-2.5 sm:h-12',
-                isCalendarOpen ? 'border-primary' : 'border-border'
-              )}
-            >
-              {format(date, 'yyyy년 MM월 dd일')}
-            </span>
+            <div className="flex gap-2">
+              {select.map((item) => (
+                <div
+                  key={item.id}
+                  onClick={item.onClick}
+                  className={clsx(
+                    'text-gray500 text-md-rg sm:text-lg-rg bg-bg200 flex h-11 w-full cursor-pointer items-center gap-3 rounded-xl border px-4 py-2.5 sm:h-12',
+                    item.isOpen ? 'border-primary' : 'border-border',
+                    item.flex
+                  )}
+                >
+                  {item.value}
+                </div>
+              ))}
+            </div>
           </div>
-          <CalendarSelect
-            isOpen={isCalendarOpen}
-            onDateChange={handleCalendarDateChange}
-            date={date}
-          />
+          {isCalendarOpen && <CalendarSelect onDateChange={handleCalendarDateChange} date={date} />}
+          {isTimeOpen && <TimePicker selectedTime={selectedTime} onTimeChange={updateTime} />}
         </div>
 
         <div className="flex flex-col gap-4">
