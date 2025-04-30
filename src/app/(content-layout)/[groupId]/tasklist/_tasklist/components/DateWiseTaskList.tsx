74a4e2fff9cb4e96b70bd151a81f2cc2b89213f9
nerte <grimza@nerteui-MacBookPro.local>
2025-04-30 17:13:57 +0900

    Chore : mockData 삭제, mockData 적용 부분 수정

--- src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/DateWiseTaskList.tsx
+++ src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/DateWiseTaskList.tsx
@@ -1,12 +1,9 @@
 'use client';
 import { useEffect, useState } from 'react';
 import clsx from 'clsx';
-// import { TaskTest1, TaskTest2, TaskTest3 } from '../mock-data-task-list-page';
 import TaskWiseTodoListItem from './TaskWiseTodoListItem';
 import axiosClient from '@/lib/axiosClient';
-import { Task, TaskList, TasksApiResponse } from '../types/task-list-page-type';
-
-// const MOCK_DATA = [TaskTest1, TaskTest2, TaskTest3];
+import { Task, TaskList } from '../types/task-list-page-type';
 
 interface Props {
   date: Date;
@@ -14,25 +11,27 @@ interface Props {
 }
 
 export default function DateWiseTaskList({ date, groupId }: Props) {
-  const [taskList, setTaskList] = useState<TaskList[]>([]);
-  const [currentTask, setCurrentTask] = useState<TaskList>(taskList[0]);
-  const [currentTasks, setCurrentTasks] = useState<Task[]>(currentTask.tasks);
+  const [taskLists, setTaskLists] = useState<TaskList[]>([]);
+  const [currentTaskList, setCurrentTaskList] = useState<TaskList>(taskLists[0]);
+  const [currentTasks, setCurrentTasks] = useState<Task[]>(currentTaskList.tasks);
 
-  const handleClickChangeCurrentTask = (task: TaskList) => {
-    setCurrentTask(task);
+  const handleClickChangeCurrentTask = (taskList: TaskList) => {
+    setCurrentTaskList(taskList);
     // setCurrentTasks(task.tasks);
   };
 
   const handleLoad = async () => {
-    const { data: taskListData } = await axiosClient(`/groups/${groupId}`);
-    const { data: tasksData } = await axiosClient(`groups/${currentTask.id}/tasks`);
-    setTaskList(taskListData.taskLists);
+    const { data: taskListsData } = await axiosClient(`/groups/${groupId}`);
+    const { data: tasksData } = await axiosClient(`groups/task-lists/${currentTaskList.id}/tasks`, {
+      params: { date },
+    });
+    setTaskLists(taskListsData.taskLists);
     setCurrentTasks(tasksData);
   };
 
   useEffect(() => {
     handleLoad();
-    setCurrentTask(taskList[0]); // date가 바뀔때마다 일단 태스크 리스트의 첫번째로 돌아가게 구현
+    setCurrentTaskList(taskLists[0]); // date가 바뀔때마다 일단 태스크 리스트의 첫번째로 돌아가게 구현
     // setCurrentTaskItem(바뀐 날짜에 의한 새로운 투두리스트...)
     //date가 바뀔 때마다 태스크안의 투두 아이템이 바뀌어야함
   }, [date]);
@@ -40,19 +39,19 @@ export default function DateWiseTaskList({ date, groupId }: Props) {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3">
-        {taskList.map((task) => {
+        {taskLists.map((taskList) => {
           return (
             <p
-              key={task.id}
-              onClick={() => handleClickChangeCurrentTask(task)}
+              key={taskList.id}
+              onClick={() => handleClickChangeCurrentTask(taskList)}
               className={clsx(
                 'text-md-md cursor-pointer',
-                task === currentTask
+                taskList === currentTaskList
                   ? 'text-gray-200 underline underline-offset-6'
                   : 'text-gray-500'
               )}
             >
-              {task.name}
+              {taskList.name}
             </p>
           );
         })}
@@ -60,8 +59,8 @@ export default function DateWiseTaskList({ date, groupId }: Props) {
       <div>
         {currentTasks.length > 0 || !currentTasks ? (
           <>
-            {currentTasks.map((item) => {
-              return <TaskWiseTodoListItem item={item} key={item.id} />;
+            {currentTasks.map((task) => {
+              return <TaskWiseTodoListItem item={task} key={task.id} />;
             })}
           </>
         ) : (
