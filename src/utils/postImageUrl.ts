cbf3c205a841f438694e9244ba4c39ff3ce2b0c9
heejin <heejin0857@naver.com>
2025-04-30 21:03:40 +0900

    Feat : add headers multipart form-data

--- src/utils/postImageUrl.ts
+++ src/utils/postImageUrl.ts
@@ -4,7 +4,9 @@ export default async function postImageUrl(file: File) {
   const image = new FormData();
   image.append('image', file);
 
-  const response = await axiosClient.post('/images/upload', image);
+  const response = await axiosClient.post('/images/upload', image, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
 
   return response.data;
 }
