2f0c3488bddd5e6a79a5d78247eea5b3c1fb1abd
nerte <grimza@nerteui-MacBookPro.local>
2025-04-30 20:53:24 +0900

    Feat : TaskLists 가 빈배열일경우, Tasks 가 빈배열일 경우 ui 분기처리

--- src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/DateWiseTaskLists.tsx
+++ src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/DateWiseTaskLists.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 export default function DateWiseTaskLists({ date, groupId }: Props) {
   const [taskLists, setTaskLists] = useState<TaskList[]>([]);
-  const [currentTaskList, setCurrentTaskList] = useState<TaskList>(taskLists[0]);
+  const [currentTaskList, setCurrentTaskList] = useState<TaskList>();
   const [currentTasks, setCurrentTasks] = useState<Task[]>([]);
 
   const handleClickChangeCurrentTaskList = (taskList: TaskList) => {
@@ -22,6 +22,19 @@ export default function DateWiseTaskLists({ date, groupId }: Props) {
 
   const fetchTaskLists = async () => {
     const { data: taskListsData } = await axiosClient(`/groups/${groupId}`);
+
+    if (taskListsData.length < 1) {
+      return (
+        <div className="flex items-center justify-center">
+          <p className="text-md-md text-gray-500">
+            아직 할 일 목록이 없습니다.
+            <br />
+            새로운 목록을 추가해주세요.
+          </p>
+        </div>
+      );
+    }
+
     setTaskLists(taskListsData.taskLists);
     setCurrentTaskList(taskListsData.taskLists[0]);
     fetchTaskListWiseTasks(taskListsData.taskLists[0]);
@@ -62,18 +75,17 @@ export default function DateWiseTaskLists({ date, groupId }: Props) {
           );
         })}
       </div>
-      <div>
+      <div className="flex w-full items-center justify-center">
         {currentTasks.length > 0 || !currentTasks ? (
-          <div className="flex flex-col gap-4">
+          <div className="flex w-full flex-col gap-4">
             {currentTasks.map((task) => {
               return <TaskWiseTodoListItem task={task} key={task.id} />;
             })}
           </div>
         ) : (
           <p className="text-md-md text-gray-500">
-            아직 할 일 목록이 없습니다.
-            <br />
-            새로운 목록을 추가해주세요.
+            아직 할 일이 없습니다.
+            <br />할 일을 추가해보세요.
           </p>
         )}
       </div>
