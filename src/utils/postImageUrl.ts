7ff0eb61c024508094fcd63b3310d30ef159fa8e
heejin <heejin0857@naver.com>
2025-04-30 20:54:01 +0900

    Feat : post image url API

---
+++ src/utils/postImageUrl.ts
@@ -0,0 +1,10 @@
+import axiosClient from '@/lib/axiosClient';
+
+export default async function postImageUrl(file: File) {
+  const image = new FormData();
+  image.append('image', file);
+
+  const response = await axiosClient.post('/images/upload', image);
+
+  return response.data;
+}
