b80f56dfa4267b1d13eab04a93fdbdb42b9bd626
heejin <heejin0857@naver.com>
2025-05-01 19:28:02 +0900

    Feat : manage group validate

---
+++ src/components/manage-group/group-validate.ts
@@ -0,0 +1,30 @@
+import { Group, GROUP_MESSAGE } from './ManageGroup';
+
+export interface State {
+  field: keyof Group;
+  message: string;
+}
+
+const manageGroupValidate = (group: Group) => {
+  const { image, name } = group;
+
+  const messageArray: State[] = [];
+
+  if (image === null || image === '') {
+    messageArray.push({
+      field: 'image',
+      message: GROUP_MESSAGE.EMPTY_GROUP_IMAGE,
+    });
+  }
+
+  if (!name.trim()) {
+    messageArray.push({
+      field: 'name',
+      message: GROUP_MESSAGE.EMPTY_GROUP_NAME,
+    });
+  }
+
+  return messageArray;
+};
+
+export default manageGroupValidate;
