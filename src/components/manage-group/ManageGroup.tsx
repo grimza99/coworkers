aa9514e6bd407d1df7a10aaf49109f58f23dd624
heejin <heejin0857@naver.com>
2025-05-01 19:28:29 +0900

    Feat : group validate

--- src/components/manage-group/ManageGroup.tsx
+++ src/components/manage-group/ManageGroup.tsx
@@ -14,32 +14,53 @@ interface MangeGroupProps {
   groupData?: Group;
 }
 
+export const GROUP_MESSAGE = {
+  EMPTY_GROUP_IMAGE: '프로필 이미지를 넣어주세요.',
+  EMPTY_GROUP_NAME: '팀 이름을 작성해 주세요.',
+  EQUAL_GROUP_NAME: '이미 존재하는 팀 이름입니다.',
+};
+
 export default function ManageGroup({ isEdit, groupData }: MangeGroupProps) {
-  const { group, handleNameChange, handleImageChange, handleAddGroupSubmit } =
+  const { group, getMessage, handleNameChange, handleImageChange, handleAddGroupSubmit } =
     useManageGroup(groupData);
 
   const groupButtonText = isEdit ? '수정하기' : '생성하기';
+
+  const imageMessage = getMessage('image');
 
   return (
     <form onSubmit={handleAddGroupSubmit} className="flex w-full flex-col gap-10">
       <div className="flex w-full flex-col gap-6">
         <FormField
           field="file-input"
+          name="image"
           label="팀 프로필"
+          required
           imageUploaderType="team"
+          isFailure={group.image === null}
+          errorMessage={imageMessage}
           image={group.image}
           onImageChange={handleImageChange}
         />
         <FormField
           field="input"
+          name="name"
           label="팀 이름"
+          required
           placeholder="팀 이름을 입력해 주세요."
+          isFailure={group.name === ''}
+          errorMessage={GROUP_MESSAGE.EMPTY_GROUP_NAME}
           value={group.name}
           onChange={handleNameChange}
         />
       </div>
       <div className="flex flex-col gap-6">
-        <Button type="submit" variant="solid" size="fullWidth">
+        <Button
+          type="submit"
+          variant="solid"
+          size="fullWidth"
+          // disabled={group.image === null || group.name === ''}
+        >
           {groupButtonText}
         </Button>
         <p className="text-lg-rg text-gray500 text-center">
--- src/components/manage-group/useManageGroup.ts
+++ src/components/manage-group/useManageGroup.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Group } from './ManageGroup';
 import postImageUrl from '@/utils/postImageUrl';
 import axiosClient from '@/lib/axiosClient';
+import manageGroupValidate, { State } from './group-validate';
 
 const INITIAL_GROUP_VALUE: Group = {
   image: null,
@@ -10,6 +11,11 @@ const INITIAL_GROUP_VALUE: Group = {
 
 export default function useManageGroup(groupData?: Group) {
   const [group, setGroup] = useState<Group>(groupData ?? INITIAL_GROUP_VALUE);
+  const [state, setState] = useState<State[]>([]);
+
+  const getMessage = (field: keyof Group) => {
+    return state.find((state) => state.field === field)?.message || '';
+  };
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGroup((prev) => ({
@@ -22,18 +28,28 @@ export default function useManageGroup(groupData?: Group) {
     const file = e.target.files?.[0];
     if (!file) return;
 
-    await postImageUrl(file)
-      .then((result) =>
-        setGroup((prev) => ({
-          ...prev,
-          image: result.url,
-        }))
-      )
-      .catch((err) => console.error(err));
+    try {
+      const result = await postImageUrl(file);
+      setGroup((prev) => ({
+        ...prev,
+        image: result.url,
+      }));
+
+      setState((prev) => prev.filter((item) => item.field !== 'image'));
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const handleAddGroupSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    const validation = manageGroupValidate(group);
+
+    if (validation.length > 0) {
+      setState(validation);
+      return;
+    }
 
     axiosClient
       .post('/groups', group)
@@ -45,5 +61,5 @@ export default function useManageGroup(groupData?: Group) {
       });
   };
 
-  return { group, handleNameChange, handleImageChange, handleAddGroupSubmit };
+  return { group, getMessage, handleNameChange, handleImageChange, handleAddGroupSubmit };
 }
