563b9ff0060817a8b9ec1f45c343a368b4cc3bd3
hyejin <hhyejin90@gmail.com>
2025-05-01 21:57:33 +0900

    Feat : 프로필 드롭다운에서 로그아웃 기능 추가(쿠키 제거 및 로그인 페이지 이동)

--- src/components/common/dropdown/ProfileItem.tsx
+++ src/components/common/dropdown/ProfileItem.tsx
@@ -1,9 +1,10 @@
+import { setClientCookie } from '@/lib/cookie/client';
 import Link from 'next/link';
 
 const PROFILE_DROPDOWN_LIST = [
   { text: '마이 히스토리', src: '/myhistory' },
   { text: '계정 설정', src: '/mypage' },
-  { text: '팀 참여', src: '/join-team' },
+  { text: '팀 참여', src: '/jointeam' },
   { text: '로그아웃', src: '' },
 ];
 
@@ -11,13 +12,14 @@ const DropDownProfileItemList = PROFILE_DROPDOWN_LIST.map((list, index) => {
   const isLast = index === PROFILE_DROPDOWN_LIST.length - 1;
 
   const handleClickLogOut = () => {
-    //로그아웃 함수
+    setClientCookie('accessToken', '');
+    window.location.href = '/login';
   };
 
   return isLast ? (
-    <div onClick={handleClickLogOut} key={list.text}>
+    <button type="button" onClick={handleClickLogOut} key={list.text}>
       {list.text}
-    </div>
+    </button>
   ) : (
     <Link href={list.src} key={list.text}>
       {list.text}
