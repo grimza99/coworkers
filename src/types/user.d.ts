842755520a7a29d470133880c0fd90c2618f224b
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-02 15:39:44 +0900

    Feat : 멤버십, role, 그룹 타입 정의

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -3,3 +3,23 @@ export interface User {
   nickname: string;
   image: string;
 }
+
+export interface Membership {
+  userId: User['id'];
+  userName: User['nickname'];
+  userImage: User['image'];
+  userEmail: string;
+  role: Role;
+  groupId: Group['id'];
+  group: Group;
+}
+
+export type Role = 'ADMIN' | 'MEMBER';
+
+interface getUserApiResponse extends User {
+  createdAt: string;
+  updatedAt: string;
+  teamId: string;
+  email: string;
+  memberships: Membership[];
+}
