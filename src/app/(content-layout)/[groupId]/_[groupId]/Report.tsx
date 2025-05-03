1a15af19a8547d6376a6d42629d8c33e215dbad1
nerte <grimza@nerteui-MacBookPro.local>
2025-05-03 23:19:31 +0900

    Feat: report 컴포넌트와 [groupId] 페이지 생성

---
+++ src/app/(content-layout)/[groupId]/_[groupId]/Report.tsx
@@ -0,0 +1,8 @@
+export default function Report() {
+  return (
+    <div className="flex w-full flex-col">
+      <p className="text-lg-md">리포트</p>
+      <div className="bg-bg200 h-56 rounded-xl lg:h-[217px]"></div>
+    </div>
+  );
+}
---
+++ src/app/(content-layout)/[groupId]/page.tsx
@@ -0,0 +1,9 @@
+import Report from './_[groupId]/Report';
+
+export default function GroupPage() {
+  return (
+    <div>
+      <Report />
+    </div>
+  );
+}
