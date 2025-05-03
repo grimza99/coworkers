70f9ee5d23253283c7be141d8c8a254905c105f9
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-03 17:43:31 +0900

    Style : 그룹 페이지의 Tasklist 아이템 컴포넌트 퍼블리싱

--- src/app/(content-layout)/[groupId]/_[groupId]/TaskListItem.tsx
+++ src/app/(content-layout)/[groupId]/_[groupId]/TaskListItem.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import TaskListItemDropdown from './TaskListItemDropdown';
+import TaskListProgressBadge from './TaskListProgressBadge';
 
 export default function TaskListItem() {
-  return <div>TaskListItem</div>;
+  const { name, totalTasks, doneTasks } = { name: '법인 설립', totalTasks: 5, doneTasks: 3 };
+  return (
+    <div className="bg-bg200 flex h-10 items-center justify-between rounded-xl">
+      <div className="flex h-full items-center gap-3">
+        <div className="bg-purple h-full w-3 rounded-l-xl"></div>
+        <div className="text-md-md">{name}</div>
+      </div>
+      <div className="mr-2 flex items-center gap-1">
+        <TaskListProgressBadge total={totalTasks} done={doneTasks} />
+        <TaskListItemDropdown />
+      </div>
+    </div>
+  );
 }
