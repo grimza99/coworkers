44058e5ff57fd8d6fc694db98b8f740ef1c52194
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-03 17:27:59 +0900

    Refactor : TaskListProgressBadge.tsx 상수 정의 및 주석 추가

--- src/app/(content-layout)/[groupId]/_[groupId]/TaskListProgressBadge.tsx
+++ src/app/(content-layout)/[groupId]/_[groupId]/TaskListProgressBadge.tsx
@@ -9,7 +9,7 @@ type TaskListProgressBadgeProps = {
 export default function TaskListProgressBadge({ total, done }: TaskListProgressBadgeProps) {
   return (
     <div className="text-primary text-md-rg bg-bg300 flex items-center gap-1 rounded-xl px-2 py-1">
-      <TaskListProgressIcon total={5} done={5} />
+      <TaskListProgressIcon total={total} done={done} />
       {`${done}/${total}`}
     </div>
   );
@@ -24,26 +24,30 @@ function TaskListProgressIcon({ total, done }: TaskListProgressIconProps) {
   if (total === done)
     return <Image src={ProgressDoneIcon} width={16} height={16} alt="모든 할 일 완료" />;
 
-  const circumference = 2 * Math.PI * 5;
+  const SIZE = 16; // 아이콘 크기
+  const CENTER = SIZE / 2; // 원의 중심
+  const RADIUS = 5; // 원의 반지름
+  const CIRCUMFERENCE = 2 * Math.PI * RADIUS; // 원의 둘레
+
   const percent = total > 0 ? done / total : 0;
-  const offset = circumference * (1 - percent);
+  const offset = CIRCUMFERENCE * (1 - percent);
 
   return (
-    <svg width="16" height="16">
+    <svg width={SIZE} height={SIZE}>
       {/* 배경 원 */}
-      <circle cx="8" cy="8" r="5" stroke="#F8FAFC" strokeWidth="2" fill="none" />
+      <circle cx={CENTER} cy={CENTER} r={RADIUS} stroke="#F8FAFC" strokeWidth="2" fill="none" />
       {/* 진행 원 */}
       <circle
-        cx="8"
-        cy="8"
-        r="5"
+        cx={CENTER}
+        cy={CENTER}
+        r={RADIUS}
         stroke="#10B981"
         strokeWidth="2"
         fill="none"
-        strokeDasharray={circumference}
+        strokeDasharray={CIRCUMFERENCE}
         strokeDashoffset={offset}
         strokeLinecap="round"
-        transform={`rotate(-90 8 8)`} // 위에서 시작하게 회전
+        transform={`rotate(-90 ${CENTER} ${CENTER})`} // 위에서 시작하게 회전
       />
     </svg>
   );
