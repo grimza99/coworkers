4f08bc6f4f2dbf8b70d306c989c27e7472e83021
nerte <grimza@nerteui-MacBookPro.local>
2025-05-04 19:49:50 +0900

    Chore : 스타일링 오타 수정

--- src/app/(content-layout)/[groupId]/_[groupId]/Report/TaskCount.tsx
+++ src/app/(content-layout)/[groupId]/_[groupId]/Report/TaskCount.tsx
@@ -15,7 +15,7 @@ export default function TaskCount({ variant, count }: Props) {
   return (
     <div className="bg-bg100 flex h-20 max-h-100 w-full items-center justify-between rounded-xl px-4 py-4">
       <div className="flex flex-col gap-1">
-        <p className="text-xs-md text-gray-300">{title}</p>
+        <p className="text-xs-md text-gray300">{title}</p>
         <p className="text-tertiary text-2xl-bold">{count}개</p>
       </div>
       <Image src={image} alt="이미지" width={40} height={40} />
