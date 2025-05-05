08dc6617b438258d03bb3fcd78640f44e951b722
nerte <grimza@nerteui-MacBookPro.local>
2025-05-05 15:22:30 +0900

    Chore : google oauth 중지, 필요없는 리소스와 버튼 삭제

--- src/components/oauth/OAuthBtnGroup.tsx
+++ src/components/oauth/OAuthBtnGroup.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Image from 'next/image';
 import kakaoIcon from '@/../public/icons/kakao-oauth-icon.svg';
-import googleIcon from '@/../public/icons/google-oauth-icon.svg';
 
 export default function OAuthButtonGroup() {
   const handleClick = () => {
@@ -10,9 +9,6 @@ export default function OAuthButtonGroup() {
 
   return (
     <div className="flex gap-4">
-      <button>
-        <Image onClick={handleClick} src={googleIcon} width={42} height={42} alt="구글" />
-      </button>
       <button>
         <Image onClick={handleClick} src={kakaoIcon} width={42} height={42} alt="카카오" />
       </button>
