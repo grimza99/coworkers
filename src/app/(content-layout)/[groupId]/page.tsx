5b19716c01c2b54077c6735817c4801715012ea4
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-06 23:47:20 +0900

    Fix : 존재하지 않는 파일 import 하는 오류 수정

--- src/app/(content-layout)/[groupId]/page.tsx
+++ src/app/(content-layout)/[groupId]/page.tsx
@@ -1,10 +1,3 @@
-import Tasklists from './_[groupId]/Tasklists/Tasklists';
-
 export default function Page() {
-  return (
-    <div>
-      그룹 페이지입니다~
-      <Tasklists />
-    </div>
-  );
+  return <div>그룹 페이지입니다~</div>;
 }
