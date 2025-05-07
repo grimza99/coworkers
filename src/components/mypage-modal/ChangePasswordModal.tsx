ab0dab72e4422a618dc133852643de84b8cb7c75
hyejin <hhyejin90@gmail.com>
2025-05-07 16:56:35 +0900

    Feat : 비밀번호 변경 모달에 validate 추가

--- src/components/mypage-modal/ChangePasswordModal.tsx
+++ src/components/mypage-modal/ChangePasswordModal.tsx
@@ -10,9 +10,23 @@ import {
 import Button from '../common/Button';
 import useModalContext from '@/components/common/modal/core/useModalContext';
 import FormField from '../common/formField';
+import { validatePassword, validateConfirmPassword } from '@/utils/validators';
+import { useState } from 'react';
 
 export default function ChangePasswordModal() {
   const { closeModal } = useModalContext();
+
+  const [formData, setFormData] = useState({
+    newPassword: '',
+    confirmPassword: '',
+  });
+
+  const setFieldValue = (key: keyof typeof formData, value: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
 
   return (
     <>
@@ -26,11 +40,31 @@ export default function ChangePasswordModal() {
                   field="input"
                   label="새 비밀번호"
                   placeholder="새 비밀번호를 입력해주세요."
+                  type="password"
+                  isFailure={!validatePassword(formData.newPassword)}
+                  errorMessage={
+                    formData.newPassword.trim() === ''
+                      ? '비밀번호를 입력해주세요.'
+                      : '비밀번호는 8자 이상 20자 이하이며 영문자, 숫자, 특수문자(!@#$%^&*)만 사용할 수 있습니다.'
+                  }
+                  value={formData.newPassword}
+                  onChange={(e) => setFieldValue('newPassword', e.target.value)}
                 />
                 <FormField
                   field="input"
                   label="새 비밀번호 확인"
                   placeholder="새 비밀번호를 다시 한 번 입력해주세요."
+                  type="password"
+                  isFailure={
+                    !validateConfirmPassword(formData.newPassword, formData.confirmPassword)
+                  }
+                  errorMessage={
+                    formData.confirmPassword.trim() === ''
+                      ? '비밀번호를 입력해주세요.'
+                      : '비밀번호가 일치하지 않습니다.'
+                  }
+                  value={formData.confirmPassword}
+                  onChange={(e) => setFieldValue('confirmPassword', e.target.value)}
                 />
               </div>
               <ModalFooter className="mt-6 w-full">
