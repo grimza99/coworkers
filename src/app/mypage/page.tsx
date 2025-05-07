150448bf65fa286521ba85cfde5a6aa54106cc6c
hyejin <hhyejin90@gmail.com>
2025-05-07 15:37:33 +0900

    Feat : Mypage에서 사용되는 모달 컴포넌트 정의

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import Button from '@/components/common/Button';
 import FormField from '@/components/common/formField';
 import axiosClient from '@/lib/axiosClient';
@@ -137,7 +136,7 @@ export default function MyTeam() {
               type="button"
               className="text-danger text-lg-md flex items-center gap-1 text-start"
             >
-              <Image src="/icons/secession.svg" width={24} height={24} alt="회원탈퇴 아이콘" />
+              <img src="/icons/secession.svg" width={24} height={24} alt="회원탈퇴 아이콘" />
               회원 탈퇴하기
             </button>
           </div>
--- src/components/mypage-modal/DeleteAccountModal.tsx
+++ src/components/mypage-modal/DeleteAccountModal.tsx
@@ -5,12 +5,11 @@ import {
   ModalHeading,
 } from '@/components/common/modal';
 import Button from '../common/Button';
-import Image from 'next/image';
 
 export default function DeleteAccountModal() {
   return (
     <ModalContainer>
-      <Image src="/icons/danger.svg" width={24} height={24} alt="경고 아이콘" />
+      <img src="/icons/danger.svg" width={24} height={24} alt="경고 아이콘" />
       <ModalHeading>회원 탈퇴를 진행하시겠어요?</ModalHeading>
       <ModalDescription>
         그룹장으로 있는 그룹은 자동으로 삭제되고, 모든 그룹에서 나가집니다.
