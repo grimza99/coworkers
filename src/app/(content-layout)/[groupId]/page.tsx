4177e875558c0cab7a56a2fe35f763859d6286c6
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-08 23:05:07 +0900

    Feat : 그룹 페이지에 3가지 섹션 추가

--- src/app/(content-layout)/[groupId]/page.tsx
+++ src/app/(content-layout)/[groupId]/page.tsx
@@ -2,6 +2,10 @@ import Image from 'next/image';
 import gearIcon from '@/../public/icons/gear-icon.svg';
 import groupThumbnailImage from '@/../public/images/group-thumbnail.png';
 import axiosServer from '@/lib/axiosServer';
+import Tasklists from './_[groupId]/Tasklists';
+// import Report from './_[groupId]/Report';
+import { Task } from '@/types/task';
+import Members from './_[groupId]/Members';
 
 export default async function Page({ params }: { params: Promise<{ groupId: string }> }) {
   const { groupId } = await params;
@@ -19,6 +23,11 @@ export default async function Page({ params }: { params: Promise<{ groupId: stri
           className="absolute right-1/4 h-16 w-auto object-contain md:right-20"
         />
         <Image src={gearIcon} width={24} height={24} alt="톱니바퀴" className="size-6" />
+      </div>
+      <div className="mt-6 mb-50 flex flex-col gap-12 lg:gap-16">
+        <Tasklists groupId={data.groupId} tasklists={data.taskLists} />
+        {/* <Report taskCount={} doneCount={} /> */}
+        <Members groupId={data.groupId} members={data.members} />
       </div>
     </main>
   );
