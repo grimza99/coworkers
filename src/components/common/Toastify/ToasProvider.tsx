63113960a5a7ddc183a867c889c22664fd77d892
heejin <heejin0857@naver.com>
2025-05-10 03:36:47 +0900

    Style : toastify style

--- src/components/common/Toastify/ToasProvider.tsx
+++ src/components/common/Toastify/ToasProvider.tsx
@@ -2,6 +2,7 @@
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './style.css';
 
 export default function ToastProvider({ children }: { children: React.ReactNode }) {
   return (
