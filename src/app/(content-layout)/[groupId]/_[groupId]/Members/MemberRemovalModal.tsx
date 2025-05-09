8c74f1ab34021afb659b5afd0d26d66fc0c85e5f
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-10 00:45:56 +0900

    Style : 멤버 내보내기 모달에서 멤버 이름이 구분되도록 텍스트 스타일 수정

--- src/app/(content-layout)/[groupId]/_[groupId]/Members/MemberRemovalModal.tsx
+++ src/app/(content-layout)/[groupId]/_[groupId]/Members/MemberRemovalModal.tsx
@@ -34,7 +34,7 @@ export default function MemberRemovalModal({ modalId, member }: MemberRemovalMod
           <ModalContainer>
             <Image src={dangerIcon} alt="경고" width={23} height={22} className="size-6" />
             <ModalHeading className="mt-4 mb-2">
-              {userName}님을 그룹에서 내보내시겠어요?
+              <span className="text-primary">{userName}</span> 님을 그룹에서 내보내시겠어요?
             </ModalHeading>
             <ModalDescription>내보낸 멤버는 다시 초대할 수 있습니다.</ModalDescription>
             <ModalFooter className="mt-6 w-70">
