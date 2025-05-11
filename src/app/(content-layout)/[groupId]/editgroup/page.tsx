ab6e7b8cc464a42f27863804dbb367ddb873d592
heejin <heejin0857@naver.com>
2025-05-11 23:47:09 +0900

    Feat : get group data & user groups

--- src/app/(content-layout)/[groupId]/editgroup/page.tsx
+++ src/app/(content-layout)/[groupId]/editgroup/page.tsx
@@ -1,21 +1,19 @@
+import getUserGroup from '@/components/manage-group/action';
 import ManageGroup from '@/components/manage-group/ManageGroup';
 import axiosServer from '@/lib/axiosServer';
-const DEFAULT_IMAGE = process.env.NEXT_PUBLIC_DEFAULT_IMAGE;
-export default async function EditGroup() {
-  const response = await axiosServer('/user/groups');
-  const groupNames = response.data.map((group: { name: string }) => group.name);
+
+export default async function EditGroup({ params }: { params: Promise<{ groupId: string }> }) {
+  const groupId = (await params).groupId;
+  const groupResponse = await axiosServer(`/groups/${groupId}`);
+  const { id, name, image } = groupResponse.data;
+  const groupData = { id, name, image };
+
+  const groupNames = await getUserGroup();
 
   return (
     <div className="flex w-full flex-col items-center gap-20">
       <h1 className="text-4xl">팀 수정하기</h1>
-      <ManageGroup
-        isEdit
-        groupData={{
-          image: DEFAULT_IMAGE!,
-          name: 'hee',
-        }}
-        groupNames={groupNames}
-      />
+      <ManageGroup groupData={groupData} groupNames={groupNames} />
     </div>
   );
 }
