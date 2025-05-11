84686476db16793e6ecc929c68a086810ddd1613
heejin <heejin0857@naver.com>
2025-05-12 01:52:38 +0900

    Feat : edit group page layout

---
+++ src/app/(content-layout)/[groupId]/editgroup/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/globals.css';
+
+export default function FormPageLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="-mt-6 flex justify-center">
+      <div className="mx-4 mt-6 w-full max-w-115 min-w-[343px] md:mx-6 md:mt-25 lg:mt-35">
+        {children}
+      </div>
+    </div>
+  );
+}
