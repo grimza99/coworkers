b16b0099cf7b195c2004914f780508790e91cf0a
heejin <heejin0857@naver.com>
2025-05-13 00:36:14 +0900

    Feat : delete group UI

--- src/app/(content-layout)/[groupId]/editgroup/page.tsx
+++ src/app/(content-layout)/[groupId]/editgroup/page.tsx
@@ -1,3 +1,4 @@
+import TrashCan from '@/assets/TrashCan';
 import getUserGroup from '@/components/manage-group/action';
 import ManageGroup from '@/components/manage-group/ManageGroup';
 import axiosServer from '@/lib/axiosServer';
@@ -11,9 +12,15 @@ export default async function EditGroup({ params }: { params: Promise<{ groupId:
   const groupNames = await getUserGroup();
 
   return (
-    <div className="flex w-full flex-col items-center gap-20">
-      <h1 className="text-4xl">팀 수정하기</h1>
-      <ManageGroup groupData={groupData} groupNames={groupNames} />
+    <div className="flex flex-col gap-6 lg:gap-10">
+      <div className="flex w-full flex-col items-center gap-20">
+        <h1 className="text-4xl">팀 수정하기</h1>
+        <ManageGroup groupData={groupData} groupNames={groupNames} />
+      </div>
+      <div className="text-danger text-lg-md flex w-fit cursor-pointer items-center justify-start gap-2">
+        <TrashCan />
+        <span>팀 삭제하기</span>
+      </div>
     </div>
   );
 }
