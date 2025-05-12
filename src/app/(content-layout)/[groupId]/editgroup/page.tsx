3959b6ee417127cdbb2ee98f9d16cac79ea011f5
heejin <heejin0857@naver.com>
2025-05-13 00:48:35 +0900

    Chore : delete layout

--- src/app/(content-layout)/[groupId]/editgroup/page.tsx
+++ src/app/(content-layout)/[groupId]/editgroup/page.tsx
@@ -12,14 +12,18 @@ export default async function EditGroup({ params }: { params: Promise<{ groupId:
   const groupNames = await getUserGroup();
 
   return (
-    <div className="flex flex-col gap-6 lg:gap-10">
-      <div className="flex w-full flex-col items-center gap-20">
-        <h1 className="text-4xl">팀 수정하기</h1>
-        <ManageGroup groupData={groupData} groupNames={groupNames} />
-      </div>
-      <div className="text-danger text-lg-md flex w-fit cursor-pointer items-center justify-start gap-2">
-        <TrashCan />
-        <span>팀 삭제하기</span>
+    <div className="-mt-6 flex justify-center">
+      <div className="mt-6 w-full max-w-115 min-w-[343px] md:mx-6 md:mt-25 lg:mt-35">
+        <div className="flex flex-col gap-6 lg:gap-10">
+          <div className="flex w-full flex-col items-center gap-20">
+            <h1 className="text-4xl">팀 수정하기</h1>
+            <ManageGroup groupData={groupData} groupNames={groupNames} />
+          </div>
+          <div className="text-danger text-lg-md flex w-fit cursor-pointer items-center justify-start gap-2">
+            <TrashCan />
+            <span>팀 삭제하기</span>
+          </div>
+        </div>
       </div>
     </div>
   );
