b954957461d4c518b4a957a535a44dc6ed28a126
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-14 23:33:35 +0900

    Feat : 게시글 폼 텍스트 입력 관리

--- src/app/(content-layout)/articles/new/page.tsx
+++ src/app/(content-layout)/articles/new/page.tsx
@@ -3,11 +3,14 @@ import Button from '@/components/common/Button';
 import FormField from '@/components/common/formField';
 import { useEffect, useState } from 'react';
 
+const isEmptyString = (str: string) => str.trim() === '';
+
 export default function Page() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [image, setImage] = useState<File | null>(null);
   const [previewImage, setPreviewImage] = useState('');
+  const canSubmit = !isEmptyString(title) && !isEmptyString(content);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -41,7 +44,9 @@ export default function Page() {
       <div className="flex items-center justify-between">
         <h1 className="text-lg-md md:text-xl-bold">게시글 쓰기</h1>
         <div className="hidden w-46 md:block">
-          <Button size="fullWidth">등록</Button>
+          <Button size="fullWidth" disabled={!canSubmit}>
+            등록
+          </Button>
         </div>
       </div>
       <div className="bg-border my-6 h-[1px] w-full"></div>
@@ -51,8 +56,7 @@ export default function Page() {
           field="input"
           placeholder="제목을 입력해주세요."
           required
-          isSuccess={false}
-          isFailure={false}
+          isFailure={isEmptyString(title)}
           errorMessage="제목을 입력해주세요."
           gapSize="16"
           labelSize="16/16"
@@ -65,8 +69,7 @@ export default function Page() {
           placeholder="내용을 입력해주세요."
           height={240}
           required
-          isSuccess={false}
-          isFailure={false}
+          isFailure={isEmptyString(content)}
           errorMessage="내용을 입력해주세요."
           gapSize="16"
           labelSize="16/16"
@@ -77,11 +80,11 @@ export default function Page() {
           label="이미지"
           field="file-input"
           imageUploaderType="board"
-          image={previewImage} // 미리보기 URL 전달
+          image={previewImage}
           onImageChange={handleImageChange}
         />
       </form>
-      <Button className="mt-10 block md:hidden" size="fullWidth">
+      <Button className="mt-10 block md:hidden" size="fullWidth" disabled={!canSubmit}>
         등록
       </Button>
     </main>
