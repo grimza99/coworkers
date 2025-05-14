0493341ca8007febf677fcaf44674053808a3a15
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-14 22:01:38 +0900

    Style : nogroup 퍼블리싱

--- src/app/(content-layout)/nogroup/page.tsx
+++ src/app/(content-layout)/nogroup/page.tsx
@@ -1,3 +1,31 @@
+import Button from '@/components/common/Button';
+import Image from 'next/image';
+
 export default function Page() {
-  return <div>소속된 그룹이 없을 때 페이지입니다.</div>;
+  return (
+    <main className="flex h-[calc(100vh-92px)] flex-col items-center justify-center gap-12 md:gap-20">
+      <div>
+        <Image
+          width={2430}
+          height={765}
+          src="/images/nogroup-main.png"
+          alt="협업하는 사람들의 모습이 표현된 메인 일러스트레이션"
+          className="mb-8 w-[312px] md:mb-12 md:w-[520px] lg:w-[810px]"
+        />
+        <h1 className="text-sm-md md:text-lg-md text-gray500 text-center">
+          아직 소속된 팀이 없습니다.
+          <br />
+          팀을 생성하거나 팀에 참여해보세요.
+        </h1>
+      </div>
+      <div className="flex w-46.5 flex-col gap-2">
+        <Button variant="solid" size="fullWidth">
+          팀 생성하기
+        </Button>
+        <Button variant="ghost-primary" size="fullWidth">
+          팀 참여하기
+        </Button>
+      </div>
+    </main>
+  );
 }
