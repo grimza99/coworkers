8d9a8528a261a20c4403643a6c261ed83475f920
heejin <heejin0857@naver.com>
2025-05-14 21:25:10 +0900

    Chore : rename props

--- src/app/(content-layout)/articles/[articleId]/_articleId/CommentList.tsx
+++ src/app/(content-layout)/articles/[articleId]/_articleId/CommentList.tsx
@@ -3,49 +3,10 @@
 import CommentItem from '@/components/comment';
 import { ArticleComment } from '@/components/comment/types';
 
-const articleComments: ArticleComment[] = [
-  {
-    id: 1,
-    content: '첫 번째 댓글',
-    createdAt: '2025-05-14T10:00:00Z',
-    updatedAt: '2025-05-14T10:00:00Z',
-    writer: {
-      id: 11,
-      nickname: '유선향',
-      image:
-        'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1844/IMG_0249.jpeg',
-    },
-  },
-  {
-    id: 2,
-    content: '두 번째 댓글',
-    createdAt: '2025-05-14T10:05:00Z',
-    updatedAt: '2025-05-14T10:05:00Z',
-    writer: {
-      id: 22,
-      nickname: '황혜진',
-      image:
-        'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1844/이미지 2024. 1. 12. 15.07.jpeg',
-    },
-  },
-  {
-    id: 3,
-    content: '세 번째 댓글',
-    createdAt: '2025-05-14T10:10:00Z',
-    updatedAt: '2025-05-14T10:15:00Z',
-    writer: {
-      id: 33,
-      nickname: '강석준',
-      image:
-        'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1844/IMG_3867.jpeg',
-    },
-  },
-];
-
-export default function CommentList() {
+export default function CommentList({ comments }: { comments: ArticleComment[] }) {
   return (
     <div className="flex flex-col gap-4">
-      {articleComments.map((comment) => (
+      {comments.map((comment) => (
         <CommentItem key={comment.id} comment={comment} onEdit={() => {}} onDelete={() => {}} />
       ))}
     </div>
--- src/app/(content-layout)/articles/page.tsx
+++ src/app/(content-layout)/articles/page.tsx
@@ -21,33 +21,14 @@ export interface Article {
   isLiked: boolean;
 }
 
-const data = {
-  id: 0,
-  title: 'title',
-  content: 'content',
-  image:
-    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1844/이미지 2024. 1. 12. 15.07.jpeg',
-  createdAt: 'createdAt',
-  updatedAt: 'updatedAt',
-  likeCount: 0,
-  commentCount: 0,
-  writer: {
-    id: 0,
-    nickname: 'nickname',
-    image:
-      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/1844/이미지 2024. 1. 12. 15.07.jpeg',
-  },
-  isLiked: false,
-};
-
 export default function DetailArticle() {
   return (
     <div className="flex h-full w-full flex-col gap-20">
-      <DetailArticleInfo detail={data} />
+      <DetailArticleInfo detail={detail} />
       <div className="flex flex-col gap-8 sm:gap-10">
         <CommentField />
         <div className="border-border border" />
-        <CommentList />
+        <CommentList comments={comments} />
       </div>
     </div>
   );
