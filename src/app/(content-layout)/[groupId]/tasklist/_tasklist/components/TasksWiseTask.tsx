403f1f2b78c262e68c4308baac7d0ab70b957b47
heejin <heejin0857@naver.com>
2025-05-19 03:41:15 +0900

    Feat : get detail task item

--- src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/TasksWiseTask.tsx
+++ src/app/(content-layout)/[groupId]/tasklist/_tasklist/components/TasksWiseTask.tsx
@@ -1,13 +1,14 @@
 'use client';
 import TaskListItem from '@/components/task-list-item/TaskListItem';
 import { format, isValid } from 'date-fns';
-import { useState } from 'react';
-import { Task } from '../types/task-type';
+import { useEffect, useState } from 'react';
+import { DetailTaskType, Task } from '../types/task-type';
 import RemoveTaskModal from './ModalContents/RemoveTaskModal';
 import { useTaskActions } from '../hooks/use-task-actions';
 import { useTaskModals } from '../hooks/use-task-modals';
 import ManageTaskItemModal from './manage-task-item-modal/MangeTaskItemModal';
 import DetailTask from '../../@detailTask/page';
+import getDetailTaskItem from '@/lib/api/detail-task-item';
 
 interface Props {
   task: Task;
@@ -19,6 +20,19 @@ export default function TasksWiseTask({ task, groupId, taskListId }: Props) {
   const [isDone, setIsDone] = useState(!!task.doneAt);
   const [isDelete, setIsDelete] = useState(false);
   const [isDetailTaskOpen, setIsDetailTaskOpen] = useState(false);
+  const [detailTask, setDetailTask] = useState<DetailTaskType>();
+
+  useEffect(() => {
+    const fetchDetailItem = async () => {
+      if (!task) return;
+      const taskId = task.id;
+      const numberGroupId = Number(groupId);
+
+      const data = await getDetailTaskItem({ groupId: numberGroupId, taskListId, taskId });
+      setDetailTask(data);
+    };
+    fetchDetailItem();
+  }, [groupId, taskListId, task]);
 
   const taskDeleteModalId = `${task.id}-delete`;
   const createOrEditModalId = task ? `${task.id}-edit` : `${taskListId}-create`;
@@ -86,7 +100,7 @@ export default function TasksWiseTask({ task, groupId, taskListId }: Props) {
             deleteTask={() => deleteTask(groupId, taskListId, task.id, setTaskToDeleteState)}
           />
           <ManageTaskItemModal
-            task={task}
+            detailTask={detailTask}
             groupId={Number(groupId)}
             taskListId={taskListId}
             isDone={isDone}
