d551c29406ea838026eaafbbbf1d103caa27bb95
heejin <heejin0857@naver.com>
2025-05-21 00:25:19 +0900

    Feat : article image

--- src/app/(content-layout)/articles/[articleId]/_articleId/components/DetailArticleInfo.tsx
+++ src/app/(content-layout)/articles/[articleId]/_articleId/components/DetailArticleInfo.tsx
@@ -22,52 +22,57 @@ export default function DetailArticleInfo({ detail }: { detail: GetArticleDetail
   const isAuthor = userId === detail.writer.id;
 
   return (
-    <div className="flex flex-col gap-12">
-      <div>
-        <div className="border-border mb-4 flex items-center justify-between border-b pb-4">
-          <h1 className="text-lg-md sm:text-2lg-md text-gray300">{detail.title}</h1>
-          {isAuthor && (
-            <DropDown
-              size="md"
-              placement="right-0"
-              options={ARTICLE_DROPDOWN_OPTIONS}
-              onSelect={() => {}}
-              dropDownOpenBtn={
-                <Image
-                  src="/icons/kebab-icon.svg"
-                  width={24}
-                  height={24}
-                  alt="kebab"
-                  className="cursor-pointer"
-                />
-              }
-            />
-          )}
-        </div>
-        <div className="text-gray400 text-xs-md flex items-center justify-between">
-          <div className="flex items-center gap-3">
-            <Image src="/icons/profile-icon.svg" width={32} height={32} alt="profile" />
-            <div>
-              <span className="text-gray100 border-bg100 mr-4 border-r pr-4">
-                {detail.writer.nickname}
-              </span>
-              <span>{formatTimeDistance(detail.createdAt)}</span>
-            </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <div className="flex items-center gap-1">
-              <Image src="/icons/comment.svg" width={16} height={16} alt="comment" />
-              <span className="text-xs-rg sm:text-md-rg">{detail.commentCount}</span>
-            </div>
-            <LikeToggleButton
-              articleId={detail.id}
-              likeCount={detail.likeCount}
-              isLiked={detail.isLiked}
-            />
-          </div>
-        </div>
+    <div className="flex w-full flex-col items-center gap-6 md:flex-row md:items-start">
+      <div className="relative aspect-[1/1] w-full max-w-125 items-center overflow-hidden rounded-2xl md:h-100 md:w-100">
+        <Image fill src={detail.image} alt="article-image" />
       </div>
-      <p className="text-md-rg sm:text-lg-rg text-gray300">{detail.content}</p>
+      <div className="flex w-full flex-1 flex-col gap-12">
+        <div>
+          <div className="border-border mb-4 flex items-center justify-between border-b pb-4">
+            <h1 className="text-lg-md sm:text-2lg-md text-gray300">{detail.title}</h1>
+            {isAuthor && (
+              <DropDown
+                size="md"
+                placement="right-0"
+                options={ARTICLE_DROPDOWN_OPTIONS}
+                onSelect={() => {}}
+                dropDownOpenBtn={
+                  <Image
+                    src="/icons/kebab-icon.svg"
+                    width={24}
+                    height={24}
+                    alt="kebab"
+                    className="cursor-pointer"
+                  />
+                }
+              />
+            )}
+          </div>
+          <div className="text-gray400 text-xs-md flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <Image src="/icons/profile-icon.svg" width={32} height={32} alt="profile" />
+              <div>
+                <span className="text-gray100 border-bg100 mr-4 border-r pr-4">
+                  {detail.writer.nickname}
+                </span>
+                <span>{formatTimeDistance(detail.createdAt)}</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="flex items-center gap-1">
+                <Image src="/icons/comment.svg" width={16} height={16} alt="comment" />
+                <span className="text-xs-rg sm:text-md-rg">{detail.commentCount}</span>
+              </div>
+              <LikeToggleButton
+                articleId={detail.id}
+                likeCount={detail.likeCount}
+                isLiked={detail.isLiked}
+              />
+            </div>
+          </div>
+        </div>
+        <p className="text-md-rg sm:text-lg-rg text-gray300">{detail.content}</p>
+      </div>
     </div>
   );
 }
