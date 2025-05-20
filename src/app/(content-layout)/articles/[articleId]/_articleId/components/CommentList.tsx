7713d9c9473630cb10c7e3d2adcf65299ffdcd59
heejin <heejin0857@naver.com>
2025-05-20 22:07:05 +0900

    Feat : comment edit UI

--- src/app/(content-layout)/articles/[articleId]/_articleId/components/CommentList.tsx
+++ src/app/(content-layout)/articles/[articleId]/_articleId/components/CommentList.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+import Textarea from '@/components/common/formField/compound/Textarea';
+import Button from '@/components/common/Button';
+import DangerModal from '@/components/DangerModal';
+import { Toast } from '@/components/common/Toastify';
 import CommentItem from '@/components/comment';
 import { ArticleComment } from '@/components/comment/types';
 import useModalContext from '@/components/common/modal/core/useModalContext';
-import DangerModal from '@/components/DangerModal';
-import { Toast } from '@/components/common/Toastify';
+
 import { deleteArticleComment } from '../action';
 
 export default function CommentList({
@@ -15,39 +18,57 @@ export default function CommentList({
   comments: ArticleComment[];
   articleId: number;
 }) {
-  const [selectedCommentId, setSelectedCommentId] = useState<number | null>(null);
+  const [commentIdToDelete, setCommentIdToDelete] = useState<number | null>(null);
+  const [commentIdToEdit, setCommentIdToEdit] = useState<number | null>(null);
   const [isPending, setIsPending] = useState(false);
   const { openModal } = useModalContext();
 
-  const deleteCommentModalId = `delete-comment-${selectedCommentId}`;
+  const deleteCommentModalId = `delete-comment-${commentIdToDelete}`;
 
   const deleteComment = () => {
-    if (selectedCommentId === null) return;
+    if (commentIdToDelete === null) return;
 
     setIsPending(true);
 
-    deleteArticleComment(articleId, selectedCommentId)
+    deleteArticleComment(articleId, commentIdToDelete)
       .catch(() => Toast.error('댓글 삭제 실패'))
       .finally(() => setIsPending(false));
   };
 
   const checkDropdownOpen = (commentId: number) => {
-    setSelectedCommentId(commentId);
+    setCommentIdToDelete(commentId);
   };
 
   return (
     <div className="mb-10 flex flex-col gap-4">
       {comments.map((comment) => {
         return (
-          <CommentItem
-            key={comment.id}
-            comment={comment}
-            onEdit={() => {}}
-            onDelete={() => {
-              openModal(deleteCommentModalId);
-            }}
-            checkDropdownOpen={() => checkDropdownOpen(comment.id)}
-          />
+          <div key={comment.id}>
+            {commentIdToEdit === comment.id ? (
+              <div className="bg-bg200 flex flex-col gap-8 rounded-lg p-4 md:px-6 md:py-5">
+                <Textarea value={comment.content} isBorder={false} />
+                <div className="flex justify-end gap-2">
+                  <button
+                    onClick={() => setCommentIdToEdit(null)}
+                    className="text-gray500 text-md-semi w-fit px-3"
+                  >
+                    취소
+                  </button>
+                  <Button size="xs" variant="ghost-primary" fontSize="14">
+                    수정하기
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <CommentItem
+                key={comment.id}
+                comment={comment}
+                onEdit={() => setCommentIdToEdit(comment.id)}
+                onDelete={() => openModal(deleteCommentModalId)}
+                checkDropdownOpen={() => checkDropdownOpen(comment.id)}
+              />
+            )}
+          </div>
         );
       })}
       <DangerModal
