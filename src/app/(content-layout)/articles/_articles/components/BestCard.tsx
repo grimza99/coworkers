ea4ae0837f641ade6f560cdb6982871d99a6e1df
hyejin <hhyejin90@gmail.com>
2025-05-20 16:29:26 +0900

    Refactor : 카드 전체 링크로 변경하고 드롭다운 제거 및 Like 버튼 readonly 처리

--- src/app/(content-layout)/articles/_articles/components/BestCard.tsx
+++ src/app/(content-layout)/articles/_articles/components/BestCard.tsx
@@ -18,45 +18,45 @@ export default function BestCard({
 }: Article & { className?: string }) {
   return (
     <div className={`relative h-[178px] w-full md:h-55 ${className}`}>
-      <div className="absolute top-4 left-6 flex items-center gap-1">
-        <Image
-          src="/icons/medal.svg"
-          alt="Best"
-          className="h-full w-full object-cover"
-          width={16}
-          height={16}
-        />
-        <span className="text-lg-semi text-white">Best</span>
-      </div>
+      <Link href={`/articles/${id}`}>
+        <div className="absolute top-4 left-6 flex items-center gap-1">
+          <Image
+            src="/icons/medal.svg"
+            alt="Best"
+            className="h-full w-full object-cover"
+            width={16}
+            height={16}
+          />
+          <span className="text-lg-semi text-white">Best</span>
+        </div>
 
-      <div className="border-bg100 bg-bg200 flex h-full flex-col gap-1 rounded-lg border px-4 pt-10 pb-4 md:gap-5 md:px-6 md:pt-12 md:pb-4">
-        <div className="flex flex-col gap-1 md:gap-3">
-          <div className="flex justify-between gap-4">
-            <Link href={`/articles/${id}`}>
+        <div className="border-bg100 bg-bg200 flex h-full flex-col gap-1 rounded-lg border px-4 pt-10 pb-4 md:gap-5 md:px-6 md:pt-12 md:pb-4">
+          <div className="flex flex-col gap-1 md:gap-3">
+            <div className="flex justify-between gap-4">
               <h3 className="text-2lg-md text-gray300 truncate">{title}</h3>
-            </Link>
-            <div className="relative h-16 w-16 shrink-0 overflow-hidden rounded-md md:h-18 md:w-18">
-              {image && image !== DEFAULT_IMAGE && (
-                <Image src={image} alt="썸네일" fill className="object-cover" />
-              )}
+              <div className="relative h-16 w-16 shrink-0 overflow-hidden rounded-md md:h-18 md:w-18">
+                {image && image !== DEFAULT_IMAGE && (
+                  <Image src={image} alt="썸네일" fill className="object-cover" />
+                )}
+              </div>
+            </div>
+            <div>
+              <span className="text-md-md text-gray400">
+                {new Date(createdAt).toLocaleDateString()}
+              </span>
             </div>
           </div>
-          <div>
-            <span className="text-md-md text-gray400">
-              {new Date(createdAt).toLocaleDateString()}
-            </span>
-          </div>
-        </div>
 
-        <div className="flex items-center justify-between">
-          <span className="text-md-md overflow-hidden text-ellipsis whitespace-nowrap">
-            {writer.nickname}
-          </span>
-          <div className="flex gap-1">
-            <LikeToggleButton articleId={id} likeCount={likeCount} />
+          <div className="flex items-center justify-between">
+            <span className="text-md-md overflow-hidden text-ellipsis whitespace-nowrap">
+              {writer.nickname}
+            </span>
+            <div className="flex gap-1">
+              <LikeToggleButton articleId={id} likeCount={likeCount} readOnly={true} />
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/app/(content-layout)/articles/_articles/components/Card.tsx
+++ src/app/(content-layout)/articles/_articles/components/Card.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Article } from '@/types/article';
-import ArticleOptionsDropdown from './ArticleOptionsDropdown';
 import LikeToggleButton from './LikeToggleButton';
 
 const DEFAULT_IMAGE = process.env.NEXT_PUBLIC_DEFAULT_IMAGE;
@@ -12,33 +11,31 @@ export default function Card(props: Article) {
   const { title, image, writer, createdAt, likeCount } = props;
 
   return (
-    <div className="border-bg100 bg-bg200 flex h-44 w-full flex-col gap-6 rounded-lg border px-8 py-6">
-      <div className="flex justify-between">
-        <Link href={`/articles/${props.id}`}>
+    <div className="border-bg100 bg-bg200 flex h-44 w-full flex-col gap-6 rounded-lg border px-9 py-6">
+      <Link href={`/articles/${props.id}`}>
+        <div className="flex justify-between">
           <h3 className="text-2lg-md text-gray300 max-w-75 truncate">{title}</h3>
-        </Link>
-        <div className="flex items-start gap-4">
-          <div className="relative h-16 w-16 shrink-0 overflow-hidden rounded-md md:h-18 md:w-18">
+
+          <div className="relative flex h-16 w-16 shrink-0 items-start overflow-hidden rounded-md md:h-18 md:w-18">
             {image && image !== DEFAULT_IMAGE && (
               <Image src={image} alt="썸네일" fill className="object-cover" />
             )}
           </div>
-          <ArticleOptionsDropdown />
         </div>
-      </div>
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <span className="text-md-md overflow-hidden text-ellipsis whitespace-nowrap">
-            {writer.nickname}
-          </span>
-          <span className="text-bg100">|</span>
-          <span className="text-md-md text-gray400">
-            {new Date(createdAt).toLocaleDateString()}
-          </span>
+        <div className="flex items-center justify-between pt-8">
+          <div className="flex items-center gap-2">
+            <span className="text-md-md overflow-hidden text-ellipsis whitespace-nowrap">
+              {writer.nickname}
+            </span>
+            <span className="text-bg100">|</span>
+            <span className="text-md-md text-gray400">
+              {new Date(createdAt).toLocaleDateString()}
+            </span>
+          </div>
+          <LikeToggleButton articleId={props.id} likeCount={likeCount} readOnly={true} />
         </div>
-        <LikeToggleButton articleId={props.id} likeCount={likeCount} />
-      </div>
+      </Link>
     </div>
   );
 }
