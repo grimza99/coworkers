48e99d389236d06c41a5ed43c9396f5305789f3a
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-21 00:57:57 +0900

    Feat: 로그아웃 시 유저 데이터 삭제

--- src/components/common/dropdown/ProfileItem.tsx
+++ src/components/common/dropdown/ProfileItem.tsx
@@ -1,5 +1,7 @@
-import { deleteClientCookie } from '@/lib/cookie/client';
 import Link from 'next/link';
+import { useUser } from '@/contexts/UserContext';
+import { deleteClientCookie } from '@/lib/cookie/client';
+import PATHS from '@/constants/paths';
 
 const PROFILE_DROPDOWN_LIST = [
   { text: '마이 히스토리', src: '/myhistory' },
@@ -8,25 +10,28 @@ const PROFILE_DROPDOWN_LIST = [
   { text: '로그아웃', src: '' },
 ];
 
-const DropDownProfileItemList = PROFILE_DROPDOWN_LIST.map((list, index) => {
-  const isLast = index === PROFILE_DROPDOWN_LIST.length - 1;
+export default function DropDownProfileItemList() {
+  const { logoutUser } = useUser();
 
   const handleClickLogOut = () => {
+    logoutUser();
     deleteClientCookie('accessToken');
     deleteClientCookie('refreshToken');
     location.reload();
-    window.location.href = '/login';
+    window.location.href = `${PATHS.LOGIN}`;
   };
 
-  return isLast ? (
-    <button type="button" onClick={handleClickLogOut} key={list.text}>
-      {list.text}
-    </button>
-  ) : (
-    <Link href={list.src} key={list.text}>
-      {list.text}
-    </Link>
-  );
-});
+  return PROFILE_DROPDOWN_LIST.map((list, index) => {
+    const isLast = index === PROFILE_DROPDOWN_LIST.length - 1;
 
-export default DropDownProfileItemList;
+    return isLast ? (
+      <button type="button" onClick={handleClickLogOut} key={list.text}>
+        {list.text}
+      </button>
+    ) : (
+      <Link href={list.src} key={list.text}>
+        {list.text}
+      </Link>
+    );
+  });
+}
