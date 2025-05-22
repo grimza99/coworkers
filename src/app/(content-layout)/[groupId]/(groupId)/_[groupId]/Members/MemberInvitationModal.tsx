bbf23dda68c9d4721b391764bf8ba3f577ce22db
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-22 12:19:04 +0900

    Feat : 멤버 이메일로 추가 기능 추가

--- src/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Members/MemberInvitationModal.tsx
+++ src/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Members/MemberInvitationModal.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { getInvitationToken } from '@/api/group';
+import { useState } from 'react';
 import Button from '@/components/common/Button';
+import FormField from '@/components/common/formField';
 import {
   ModalCloseButton,
   ModalContainer,
@@ -11,17 +12,22 @@ import {
   ModalPortal,
 } from '@/components/common/modal';
 import useModalContext from '@/components/common/modal/core/useModalContext';
+import { postMemberAction } from '@/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Members/actions';
 import { Toast } from '@/components/common/Toastify';
+import { getInvitationToken } from '@/api/group';
 import { Group } from '@/types/group';
+import { validateEmail, validateEmptyValue } from '@/utils/validators';
 
 interface MemberInvitationModalProps {
   modalId: string;
   groupId: Group['id'];
 }
 
-// @TODO: '이메일로 초대' API 활용? /{teamId}/groups/{id}/member
 export default function MemberInvitationModal({ modalId, groupId }: MemberInvitationModalProps) {
   const { closeModal } = useModalContext();
+  const [isTokenMethod, setIsTokenMethod] = useState(true);
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const copyInvitationTokenToClipboard = async () => {
     try {
@@ -34,12 +40,20 @@ export default function MemberInvitationModal({ modalId, groupId }: MemberInvita
     }
   };
 
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const addMember = async (email: string) => {
+    postMemberAction(groupId, email);
+  };
+
   return (
     <ModalPortal modalId={modalId}>
       <ModalOverlay modalId={modalId}>
         <ModalContainer>
           <ModalCloseButton modalId={modalId} />
-          <ModalHeading className="mb-2">멤버 초대</ModalHeading>
+          <ModalHeading className="mb-2">초대 링크 복사</ModalHeading>
           <ModalDescription>그룹에 참여할 수 있는 링크를 복사합니다.</ModalDescription>
           <ModalFooter className="mt-5 w-70">
             <Button
@@ -53,6 +67,29 @@ export default function MemberInvitationModal({ modalId, groupId }: MemberInvita
               링크 복사하기
             </Button>
           </ModalFooter>
+
+          <ModalHeading className="mb-2">이메일로 멤버 추가</ModalHeading>
+          <FormField
+            field="input"
+            placeholder="추가할 유저의 이메일을 입력해 주세요."
+            isSuccess={!validateEmptyValue(email) && validateEmail(email)}
+            isFailure={validateEmptyValue(email) || !validateEmail(email)}
+            errorMessage="올바른 이메일을 입력해 주세요."
+            value={email}
+            onChange={handleChangeEmail}
+            disabled={isLoading}
+          />
+
+          <Button
+            variant="solid"
+            size="fullWidth"
+            onClick={() => {
+              addMember(email);
+              closeModal(modalId);
+            }}
+          >
+            추가하기
+          </Button>
         </ModalContainer>
       </ModalOverlay>
     </ModalPortal>
--- src/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Members/actions.ts
+++ src/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Members/actions.ts
@@ -13,3 +13,15 @@ export async function removeMemberAction(groupId: Group['id'], userId: User['id'
     return { success: false, message: '멤버 삭제에 실패했습니다.' };
   }
 }
+
+export async function postMemberAction(groupId: Group['id'], userEmail: string) {
+  try {
+    const res = await axiosServer.post(`/groups/${groupId}/member`, { userEmail });
+    if (res.status === 204) {
+      revalidateTag('group');
+    }
+    return { success: true, message: '멤버가 성공적으로 추가되었습니다.' };
+  } catch {
+    return { success: false, message: '멤버 추가에 실패했습니다.' };
+  }
+}
