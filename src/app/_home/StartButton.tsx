30c0c2c566df4ae33566a9e33b8b95fc127bb9a5
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-22 12:17:48 +0900

    Fix : 소속 그룹 없는 경우 startbutton 수정

--- src/app/_home/StartButton.tsx
+++ src/app/_home/StartButton.tsx
@@ -5,25 +5,36 @@ import Button from '@/components/common/Button';
 import Spinner from '@/components/common/spinner';
 import PATHS from '@/constants/paths';
 
-export default function StartButton({ className, children }: React.ComponentProps<'button'>) {
+export default function StartButton({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<'button'>) {
   const { memberships, isLoading } = useUser();
 
   if (isLoading) {
     return (
-      <Button variant="gradient" fontSize="16" size="xl" className={className} disabled>
+      <Button variant="gradient" fontSize="16" size="xl" className={className} {...props} disabled>
         <Spinner className="flex size-6 items-center justify-center" />
       </Button>
     );
   }
 
-  const determinedHref =
-    memberships && memberships.length > 0 && memberships[0]?.group?.id
-      ? PATHS.getGroupPath(memberships[0].group.id)
-      : PATHS.LOGIN;
+  let determinedHref: string;
+
+  if (!memberships) {
+    determinedHref = PATHS.LOGIN;
+  } else if (memberships.length === 0) {
+    determinedHref = PATHS.NOGROUP;
+  } else if (memberships[0].group.id) {
+    determinedHref = PATHS.getGroupPath(memberships[0].group.id);
+  } else {
+    determinedHref = PATHS.LOGIN;
+  }
 
   return (
-    <Link href={determinedHref} className={className}>
-      <Button variant="gradient" fontSize="16" size="xl">
+    <Link href={determinedHref}>
+      <Button variant="gradient" fontSize="16" size="xl" className={className} {...props}>
         {children}
       </Button>
     </Link>
