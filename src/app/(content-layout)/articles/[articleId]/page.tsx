1078efdcf434035d382130bad1adf2c55432ef65
heejin <heejin0857@naver.com>
2025-05-24 04:27:42 +0900

    Feat : scroll top button

--- src/app/(content-layout)/articles/[articleId]/page.tsx
+++ src/app/(content-layout)/articles/[articleId]/page.tsx
@@ -2,6 +2,7 @@ import { getDetailArticle, getArticleComments } from './_articleId/action';
 import CommentField from './_articleId/components/CommentField';
 import CommentList from './_articleId/components/CommentList';
 import DetailArticleInfo from './_articleId/components/DetailArticleInfo';
+import ScrollTopButton from './_articleId/components/ScrollTopButton';
 
 export default async function DetailArticle({
   params,
@@ -13,7 +14,7 @@ export default async function DetailArticle({
   const initialComments = await getArticleComments(articleId, 10);
 
   return (
-    <div className="flex h-full w-full flex-col gap-20">
+    <div className="relative flex h-full w-full flex-col gap-20">
       <DetailArticleInfo detail={detail} />
       <div className="flex flex-col gap-8 sm:gap-10">
         <CommentField articleId={articleId} />
@@ -24,6 +25,7 @@ export default async function DetailArticle({
           articleId={articleId}
         />
       </div>
+      <ScrollTopButton />
     </div>
   );
 }
