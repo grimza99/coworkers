03ed3f689e8d43cfae9279ef94b9d7ec9d0552dd
heejin <heejin0857@naver.com>
2025-05-24 04:46:21 +0900

    Feat : metadata

--- src/app/(content-layout)/articles/[articleId]/page.tsx
+++ src/app/(content-layout)/articles/[articleId]/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from 'next';
 import { getDetailArticle, getArticleComments } from './_articleId/action';
 import CommentField from './_articleId/components/CommentField';
 import CommentList from './_articleId/components/CommentList';
 import DetailArticleInfo from './_articleId/components/DetailArticleInfo';
 import ScrollTopButton from './_articleId/components/ScrollTopButton';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: '자유게시판 | Coworkers',
+    description: '자유롭게 게시글을 작성하고 소통할 수 있는 공간입니다.',
+  };
+}
 
 export default async function DetailArticle({
   params,
