af48f8918083f08c38c710ae17cc297f8396dca5
nerte <grimza@nerteui-MacBookPro.local>
2025-05-24 00:48:57 +0900

    Chore : 로딩스피너, 바운싱 도트,적용

--- src/app/(form-layout)/reset-password/page.tsx
+++ src/app/(form-layout)/reset-password/page.tsx
@@ -1,14 +1,9 @@
 import { Suspense, use } from 'react';
 import ResetPasswordForm from './_reset-password/components/ResetPasswordForm';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '비밀번호 재설정 | Coworkers',
-  description: 'Coworkers 비밀번호 재설정 하기',
-};
+import Spinner from '@/components/common/loading/Spinner';
 
 interface Props {
-  searchParams: Promise<{ [token: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ [token: string]: string | undefined }>;
 }
 export default function ResetPasswordPage({ searchParams }: Props) {
   const { token } = use(searchParams);
@@ -16,7 +11,7 @@ export default function ResetPasswordPage({ searchParams }: Props) {
   return (
     <div className="flex flex-col gap-6 text-center md:gap-20">
       <p className="text-2xl-md lg:text-4xl">비밀번호 재설정</p>
-      <Suspense fallback={<div>로딩스피너 들어갈 영역</div>}>
+      <Suspense fallback={<Spinner />}>
         <ResetPasswordForm token={token} />
       </Suspense>
     </div>
