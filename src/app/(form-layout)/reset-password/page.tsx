a3b9f6b6fb47484f9f54aa124dba647d160043fe
nerte <grimza@nerteui-MacBookPro.local>
2025-05-24 01:03:32 +0900

    Chore : 스피너 스타일 수정

--- src/app/(form-layout)/reset-password/page.tsx
+++ src/app/(form-layout)/reset-password/page.tsx
@@ -11,7 +11,7 @@ export default function ResetPasswordPage({ searchParams }: Props) {
   return (
     <div className="flex flex-col gap-6 text-center md:gap-20">
       <p className="text-2xl-md lg:text-4xl">비밀번호 재설정</p>
-      <Suspense fallback={<Spinner />}>
+      <Suspense fallback={<Spinner className="text-4xl" />}>
         <ResetPasswordForm token={token} />
       </Suspense>
     </div>
