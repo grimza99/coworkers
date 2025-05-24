1f90c39ec2c6dae8da94b4751e69a7260c7cc763
heejin <heejin0857@naver.com>
2025-05-25 02:54:32 +0900

    Style : placement

--- src/components/comment/CommentItemDropdown.tsx
+++ src/components/comment/CommentItemDropdown.tsx
@@ -36,7 +36,7 @@ export default function CommentItemDropdown({
         }
         options={ITEM_DROPDOWN_VALUE}
         onSelect={handleClickDropdownOption}
-        placement="top-6 right-[14px]"
+        placement="top-5 right-[10px]"
       />
     )
   );
