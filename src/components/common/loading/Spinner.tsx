2fe76076aaf4dabc1320f09b572c2286592275ea
Seok-jun.Kang <kangsj0327@gmail.com>
2025-05-24 20:56:58 +0900

    Style : spinner 스타일 수정 (색상, 테두리)

--- src/components/common/loading/Spinner.tsx
+++ src/components/common/loading/Spinner.tsx
@@ -4,27 +4,27 @@ type SpinnerProps = {
   className?: string;
 };
 
-export default function Spinner({ className = '' }: SpinnerProps) {
+export default function Spinner({ className = 'text-primary' }: SpinnerProps) {
   return (
     <svg
-      className={clsx('animate-spin text-white', className)}
+      className={clsx('animate-spin', className)}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
     >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
       <circle
-        className="opacity-25"
+        className="opacity-75"
+        stroke="currentColor"
+        strokeWidth="4"
         cx="12"
         cy="12"
         r="10"
-        stroke="currentColor"
-        strokeWidth="4"
-      ></circle>
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+        fill="none"
+        strokeDasharray={2 * Math.PI * 10}
+        strokeDashoffset={2 * Math.PI * 10 * 0.7}
+        strokeLinecap="round"
+      />
     </svg>
   );
 }
