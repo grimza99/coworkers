5ab896141763c619628d66008e69937ff2220481
nerte <grimza@nerteui-MacBookPro.local>
2025-05-30 20:11:37 +0900

    Feat : 사이드로 나온 디테일 태스크에서 확장 버튼 클릭시 디테일 태스크 페이지로 이동

--- src/app/(content-layout)/[groupId]/tasklist/@detailTask/(..)tasks/[taskId]/page.tsx
+++ src/app/(content-layout)/[groupId]/tasklist/@detailTask/(..)tasks/[taskId]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useCallback, useEffect, useRef } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import DetailTask from '../../_components/DetailTask';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   groupId: string;
@@ -17,6 +18,7 @@ interface Props {
 
 export default function DetailTaskContainer({ taskId, isOpen, closeDetailTask, ...props }: Props) {
   const detailTaskRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
 
   const closingDetailTaskOutsideClick = useCallback(
     (e: MouseEvent) => {
@@ -50,9 +52,19 @@ export default function DetailTaskContainer({ taskId, isOpen, closeDetailTask, .
           className="bg-bg200 animate-detail-task fixed top-15 right-0 z-500 flex h-[calc(100%-60px)] w-full flex-col gap-25 px-4 py-4 md:max-w-[700px] md:gap-45.5 md:px-6 md:py-6 lg:max-w-[779px] lg:px-10 lg:py-10"
         >
           <div className="relative flex h-full flex-col gap-4">
-            <button onClick={() => closeDetailTask()}>
-              <Image src="/icons/close.svg" alt="x" width={24} height={24} />
-            </button>
+            <div className="flex gap-2">
+              <button
+                onClick={() => {
+                  router.back();
+                  closeDetailTask();
+                }}
+              >
+                <Image src="/icons/close.svg" alt="x" width={24} height={24} />
+              </button>
+              <button onClick={() => window.location.reload()}>
+                <Image src="/icons/expand-icon.svg" alt="확장아이콘" width={16} height={16} />
+              </button>
+            </div>
             <ErrorBoundary fallback={<div>해당 태스크를 불러올 수 없습니다.</div>}>
               <DetailTask taskId={taskId} {...props} />
             </ErrorBoundary>
