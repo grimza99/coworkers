93475d0d8a2e4bd0d58484390b663a811d170171
Seok-jun.Kang <kangsj0327@gmail.com>
2025-06-04 15:33:59 +0900

    Fix : tasklist 생성 모달 경로 직접 방문 시 not found 렌더링

--- src/app/[...modal]/page.tsx
+++ src/app/[...modal]/page.tsx
@@ -1,13 +1,5 @@
 'use client';
-import { useState } from 'react';
-import { useParams, useRouter } from 'next/navigation';
-import Button from '@/components/common/Button';
-import FormField from '@/components/common/formField';
-import Modal from '@/components/common/modal/newModal';
-import BouncingDots from '@/components/common/loading/BouncingDots';
-import { validateEmptyValue } from '@/utils/validators';
-import { createTasklistAction } from '@/app/(content-layout)/[groupId]/(groupId)/_[groupId]/Tasklists/actions';
 
-export default function MymodalPage() {
-  return <>zz</>;
-}
+import notFound from '@/app/not-found';
+
+export default notFound;
