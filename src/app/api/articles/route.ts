251c5e5c2ad4f4b7852f3466b474693db5e7f380
grimza99 <gbtmxlf@naver.com>
2026-08-22 16:54:26 +0900

    feat : create post api bff layer

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -2,6 +2,7 @@ import { BACKEND_API } from '@/constants/api';
 import { requestHeader } from '@/lib/api/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
+/**---------------------------------------------------- 게시글 조회 ----------------------------------------------------- */
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const queryString = searchParams.toString();
@@ -19,6 +20,30 @@ export async function GET(request: NextRequest) {
     const data = await res.json();
 
     return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error('User Fetch Error:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
+
+/**---------------------------------------------------- 게시글 작성 ----------------------------------------------------- */
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  try {
+    const res = await fetch(`${BACKEND_API.article.create}`, {
+      method: 'POST',
+      headers: requestHeader(request),
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      return NextResponse.json({ message: 'Fetch failed' }, { status: res.status });
+    }
+
+    const data = await res.json();
+
+    return NextResponse.json(data, { status: 201 });
   } catch (error) {
     console.error('User Fetch Error:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,11 +4,12 @@ const BACKEND_BASEURL = process.env.NEXT_PUBLIC_API_URL;
 export const BFF_API = {
   entry: BFF_BASEURL,
   user: '/user',
-  article: { list: '/articles' },
+  article: { list: '/articles', create: '/articles' },
 };
 export const BACKEND_API = {
   user: `${BACKEND_BASEURL}${BFF_API.user}`,
   article: {
     list: `${BACKEND_BASEURL}${BFF_API.article.list}`,
+    create: `${BACKEND_BASEURL}${BFF_API.article.create}`,
   },
 };
